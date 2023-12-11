25f533cbbbf89ad35575db0304b2c7a0614f8e68
Klaudia Wróblewska <kwroblewska0410@gmail.com>
2023-12-11 14:51:06 +0100

    clean code

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,8 +1,7 @@
-import { TaskModel } from "../models/TaskModel";
 //asyncHandler: We use asyncHandler to simplify error handling in asynchronous code. It helps us avoid writing repetitive try-catch blocks by automatically catching errors and passing them to our error handling middleware. This makes our code cleaner and more readable, reducing the risk of unhandled exceptions that could crash the server.
-import asyncHandler from "express-async-handler";
+import asyncHandler from 'express-async-handler';
 // We need to import the userModel to check for the famous accesstoken
-import { UserModel } from "../models/UserModel";
+import { UserModel } from '../models/UserModel';
 
 // desciption: Get Tasks
 // route: /get
@@ -12,7 +11,7 @@ export const getTasksController = asyncHandler(async (req, res) => {
   const userStorage = req.user;
   // Use the TaskModel to find all tasks associated with the logged-in user
   await TaskModel.find({ user: userStorage })
-    .sort("-createdAt")
+    .sort('-createdAt')
     .then((result) => res.json(result)) // Respond with the found tasks in JSON format
     .catch((err) => res.json(err)); // Handle any errors that occur during the operation
 });
@@ -25,7 +24,7 @@ export const addTaskController = asyncHandler(async (req, res) => {
     // Extract the task data from the request body
     const { task } = req.body;
     // Extract the accessToken from the request object, but it is not going to be from the req.body but, its going to be from the req.header
-    const accessToken = req.header("Authorization"); // we are requesting the Authorization key from the headerObject
+    const accessToken = req.header('Authorization'); // we are requesting the Authorization key from the headerObject
     // get the user and matchIt with the user from the db - remmeber that we are using the accessToken to do so :)
     const userFromStorage = await UserModel.findOne({
       accessToken: accessToken,
@@ -51,16 +50,12 @@ export const updateTaskController = asyncHandler(async (req, res) => {
   // Use TaskModel to find and update a task by its ID, marking it as done
   // Use TaskModel to delete all tasks in the database
   // Extract the accessToken from the request object, but it is not going to be from the req.body but, its going to be from the req.header
-  const accessToken = req.header("Authorization"); // we are requesting the Authorization key from the headerObject
+  const accessToken = req.header('Authorization'); // we are requesting the Authorization key from the headerObject
   // get the user and matchIt with the user from the db - remmeber that we are using the accessToken to do so :)
   const userFromStorage = await UserModel.findOne({
     accessToken: accessToken,
   });
-  await TaskModel.findByIdAndUpdate(
-    { _id: id },
-    { done: true },
-    { user: userFromStorage }
-  )
+  await TaskModel.findByIdAndUpdate({ _id: id }, { done: true }, { user: userFromStorage })
     .then((result) => res.json(result)) // Respond with the updated task in JSON format
     .catch((err) => res.json(err)); // Handle any errors that occur during the operation
 });
@@ -71,7 +66,7 @@ export const updateTaskController = asyncHandler(async (req, res) => {
 export const deleteAllTasksController = asyncHandler(async (req, res) => {
   // Use TaskModel to delete all tasks in the database
   // Extract the accessToken from the request object, but it is not going to be from the req.body but, its going to be from the req.header
-  const accessToken = req.header("Authorization"); // we are requesting the Authorization key from the headerObject
+  const accessToken = req.header('Authorization'); // we are requesting the Authorization key from the headerObject
   // get the user and matchIt with the user from the db - remmeber that we are using the accessToken to do so :)
   const userFromStorage = await UserModel.findOne({
     accessToken: accessToken,
@@ -79,9 +74,9 @@ export const deleteAllTasksController = asyncHandler(async (req, res) => {
   await TaskModel.deleteMany({ user: userFromStorage })
     .then((result) =>
       res.json({
-        message: "All tasks deleted",
+        message: 'All tasks deleted',
         deletedCount: result.deletedCount,
-      })
+      }),
     ) // Respond with a success message and the count of deleted tasks
     .catch((err) => res.status(500).json(err)); // Handle any errors that occur during the operation
 });
@@ -97,11 +92,11 @@ export const deleteSpecificTaskController = asyncHandler(async (req, res) => {
     .then((result) => {
       if (result) {
         res.json({
-          message: "Task deleted successfully",
+          message: 'Task deleted successfully',
           deletedTask: result,
         }); // Respond with a success message and the deleted task
       } else {
-        res.status(404).json({ message: "Task not found" }); // Respond with a 404 error if the task is not found
+        res.status(404).json({ message: 'Task not found' }); // Respond with a 404 error if the task is not found
       }
     })
     .catch((err) => res.status(500).json(err)); // Handle any errors that occur during the operation
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 // Import necessary libraries and modules
-import express from "express"; // Import the Express.js framework
-import cors from "cors"; // Import the CORS middleware
-import dotenv from "dotenv"; // Import dotenv for environment variables
+import express from 'express'; // Import the Express.js framework
+import cors from 'cors'; // Import the CORS middleware
+import dotenv from 'dotenv'; // Import dotenv for environment variables
 dotenv.config(); // Load environment variables from the .env file
-import taskRoutes from "./routes/taskRoutes"; // Import custom task controlled-routes
-import userRoutes from "./routes/userRoutes"; // Import custom user routes
-import { connectDB } from "./config/db"; // Import database connection function (not used here)
+
+import userRoutes from './routes/userRoutes'; // Import custom user routes
+import { connectDB } from './config/db'; // Import database connection function (not used here)
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 const port = process.env.PORT; // Set the port number for the server
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data
 
 // Use the routes for handling API requests
 // ROUTES - These routes USE controller functions ;)
-app.use(taskRoutes); // Use the task-controlled routes for task-related requests
+
 app.use(userRoutes); // Use the user-controlled routes for user-related requests
 
 // Connection to the database through Mongoose
