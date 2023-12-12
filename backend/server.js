e1e4007f22d2b5cfad154e866a2ce1ab1a8599b1
Minadarabi <minaadabirii@gmail.com>
2023-12-12 14:31:59 -0800

    set up server

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,30 @@
-// Import necessary libraries and modules
-import express from "express"; // Import the Express.js framework
-import cors from "cors"; // Import the CORS middleware
-import dotenv from "dotenv"; // Import dotenv for environment variables
-dotenv.config(); // Load environment variables from the .env file
-import taskRoutes from "./routes/taskRoutes"; // Import custom task controlled-routes
-import userRoutes from "./routes/userRoutes"; // Import custom user routes
-import { connectDB } from "./config/db"; // Import database connection function (not used here)
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-const port = process.env.PORT; // Set the port number for the server
-const app = express(); // Create an instance of the Express application
+import express from "express"; 
+import cors from "cors"; 
+import dotenv from "dotenv"; 
+dotenv.config(); 
+// import advertRoutes from "./routes/advertRoutes"
+// import messageRoutes from "./routes/messageRoutes"
+// import userRoutes from "./routes/userRoutes"
+import { connectDB } from "./config/db"; 
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors()); // Enable CORS (Cross-Origin Resource Sharing)
-app.use(express.json()); // Parse incoming JSON data
-app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data
 
-// Use the routes for handling API requests
-// ROUTES - These routes USE controller functions ;)
-app.use(taskRoutes); // Use the task-controlled routes for task-related requests
-app.use(userRoutes); // Use the user-controlled routes for user-related requests
 
-// Connection to the database through Mongoose
+const port = process.env.PORT; 
+const app = express();
+
+
+app.use(cors()); 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false })); 
+
+
+//app.use(taskRoutes); // Use the task-controlled routes for task-related requests
+//app.use(userRoutes); // Use the user-controlled routes for user-related requests
+
 connectDB();
 
-// Start the server and listen for incoming requests on the specified port
+
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`); // Display a message when the server is successfully started
+  console.log(`Server running on http://localhost:${port}`); 
 });
