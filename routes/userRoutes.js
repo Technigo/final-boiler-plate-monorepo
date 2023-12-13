ee4348fb66d57d50a7c424b1e835481378e7752d
Frida <139373483+FridaKristian@users.noreply.github.com>
2023-12-13 11:14:28 +0100

    Removed and re-wrote some comments

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,20 +1,15 @@
-// Import the necessary modules and functions
-import express from "express";
+import express from "express"
 import {
   registerUserController,
   loginUserController,
-} from "../controllers/userController"; // Import controller functions for user registration and login
+} from "../controllers/userController"
 
-// Create an instance of the Express router
 const router = express.Router();
 
 // REGISTER ROUTE: Handle user registration
-router.post("/register", registerUserController); // When a POST request is made to /register, execute the registerUserController function
+router.post("/users/register", registerUserController)
 
 // LOGIN ROUTE: Handle user login
-router.post("/login", loginUserController); // When a POST request is made to /login, execute the loginUserController function
+router.post("/users/login", loginUserController)
 
-// Export the router for use in the main application
-export default router;
-
-// In summary, this file sets up routes using the Express router for user registration and login operations. It associates each route with the corresponding controller function. These routes define the API endpoints for handling user registration and login within the application.
+export default router
--- server.js
+++ server.js
@@ -1,30 +1,28 @@
-// Import necessary libraries and modules
-import express from 'express'; // Import the Express.js framework
-import cors from 'cors'; // Import the CORS middleware
-import dotenv from 'dotenv'; // Import dotenv for environment variables
-dotenv.config(); // Load environment variables from the .env file
+import express from 'express' // framework: Express.js
+import cors from 'cors' // CORS middleware
+import dotenv from 'dotenv' // Import dotenv
+dotenv.config() // Loads environment variables from the .env file
 
-import userRoutes from './routes/userRoutes'; // Import custom user routes
-import { connectDB } from './config/db'; // Import database connection function (not used here)
+import userRoutes from './routes/userRoutes'
+import { connectDB } from './config/db' // Import database connection function (not used here)???
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
-const port = process.env.PORT; // Set the port number for the server
-const app = express(); // Create an instance of the Express application
+const port = process.env.PORT || 8080 // Set the port number for the server
+const app = express() // Create an instance of the Express application
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors()); // Enable CORS (Cross-Origin Resource Sharing)
-app.use(express.json()); // Parse incoming JSON data
-app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data
+app.use(cors()) // CORS = Cross-Origin Resource Sharing
+app.use(express.json()) // Parse incoming data in JSON-format
+app.use(express.urlencoded({ extended: false })) // Parse URL-encoded data using the querystring library. Parses simple objects and arrays
 
 // Use the routes for handling API requests
 // ROUTES - These routes USE controller functions ;)
-
-app.use(userRoutes); // Use the user-controlled routes for user-related requests
+app.use(userRoutes) // Use the user-controlled routes for user-related requests
 
 // Connection to the database through Mongoose
-connectDB();
+connectDB()
 
 // Start the server and listen for incoming requests on the specified port
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`); // Display a message when the server is successfully started
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
