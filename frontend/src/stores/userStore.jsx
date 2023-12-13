aa367ce2daa874a067dc72dfe4ececdac4303cfe
nguyenvuhang@gmail.com <nguyenvuhang@gmail.com>
2023-12-13 23:12:24 +0100

    update userStore with functions to handle user's profile update and deletion

--- frontend/src/stores/userStore.jsx
+++ frontend/src/stores/userStore.jsx
@@ -1,34 +1,37 @@
 // Import the 'create' function from the 'zustand' library.
 import { create } from "zustand";
+import { UserModel } from "../../../backend/models/UserModel";
 
 // Get the backend API endpoint from the environment variables.
 const apiEnv = import.meta.env.VITE_BACKEND_API;
 
 // Create a Zustand store for user-related state and actions.
-export const userStore = create((set, get) => ({
-  // Initialize username state.
+export const userStore = create((set) => ({
   username: "",
-  // Define a function to set the username state.
   setUsername: (username) => set({ username }),
 
-  // Initialize email state.
   email: "",
-  // Define a function to set the email state.
   setEmail: (email) => set({ email }),
 
-  // Initialize password state.
   password: "",
-  // Define a function to set the password state.
   setPassword: (password) => set({ password }),
 
-  // Initialize accessToken state with null.
+  location: "",
+  setLocation: (location) => set({ location }),
+
+  introduction: "",
+  setIntroduction: (introduction) => set({ introduction }),
+
+  products: [],
+  setProducts: (products) => set({ products }),
+
+  userId: null,
+  setUserId: (userId) => set({ userId }),
+
   accessToken: null,
-  // Define a function to set the accessToken state.
   setAccessToken: (token) => set({ accessToken: token }),
 
-  // Initialize isLoggedIn state with false.
   isLoggedIn: false,
-  // Define a function to set the isLoggedIn state.
   setIsLoggedIn: (isLoggedIn) => set({ isLoggedIn }),
 
   // FUNCTION TO REGISTER USERS
@@ -49,20 +52,20 @@ export const userStore = create((set, get) => ({
         body: JSON.stringify({ email, username, password }),
       });
 
-      // Parse the response data as JSON.
+      // Parse the response data as JSON
       const data = await response.json();
       if (data.success) {
-        // Update the username state.
+        // Update the username state
         set({ username });
-        // Display a success alert.
+        // Display a success alert
         alert("Signup successful!");
         console.log("Signing up with:", username);
       } else {
-        // Display an error message from the server or a generic message.
+        // Display an error message from the server or a generic message
         alert(data.response || "Signup failed");
       }
     } catch (error) {
-      // Handle and log any signup errors.
+      // Handle and log any signup errors
       console.error("Signup error:", error);
       alert("An error occurred during signup");
     }
@@ -111,8 +114,64 @@ export const userStore = create((set, get) => ({
     }
   },
 
-  // FUNCTION TO HANDLE USER UPDATE
+  // FUNCTION TO DISPLAY USER PROFILE
+  handleProfileDisplay: async (isLoggedIn, userId) => {
+    // Check if the user is logged in and display message if they are not
+    if (!isLoggedIn) {
+      alert("Please log in to see your profile");
+    } else {
+      try {
+        // If they are logged in, send GET request to the user endpoint to retrieve user data
+        const userToBeDisplayed = await UserModel.findById(userId);
 
+        
+      } catch (error) {
+
+      };
+    }
+    
+  },
+
+  // FUNCTION TO HANDLE USER UPDATE
+  handleProfileUpdate: async (isLoggedIn, userId, email, password, location, introduction, products) => {
+    // Check if the user is logged in and display message if they are not
+    if (!isLoggedIn) {
+      alert("Please log in to update your profile");
+    } else {
+      // If they are logged in, send a POST request to the update endpoint with user data. 
+      try { 
+        const response = await fetch(`${apiEnv}/update/${userId}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password, location, introduction, products }),
+        });
+
+        // Parse the response data as JSON.
+        const data = await response.json();
+        if (data.success) {
+          // Update relevant states
+          set({ 
+            email, 
+            password, 
+            location, 
+            introduction, 
+            products 
+          });
+          // Display a success alert
+          alert("User profile update successful!");
+        } else {
+          // Display an error message from the server or a generic message
+          alert(data.response || "User profile update failed");
+        }
+      } catch (error) {
+        // Handle and log any login errors
+        console.error("Update error:", error);
+        alert("An error occurred during profile update");
+      }
+    }
+  },
 
   // FUNCTION TO HANDLE USER LOGOUT
   handleLogout: () => {
@@ -123,9 +182,25 @@ export const userStore = create((set, get) => ({
     // Additional logout logic can be added here if needed.
   },
 
-  // FUNCTION TO HANDLE USER ACCOUNT REMOVAL
-
-  
+  // FUNCTION TO HANDLE USER ACCOUNT DELETION
+  handleAccountDeletion: () => {
+    // Clear user information
+    set({ 
+      username: "", 
+      email: "",
+      password: "",
+      location: "",
+      introduction: "",
+      products: [],
+      userId: null,
+      accessToken: null, 
+      isLoggedIn: false 
+    });
+    // Remove the accessToken from localStorage.
+    localStorage.removeItem("accessToken");
+    // Show an alert that the account has been deleted
+    alert("Your account has been deleted. Feel free to create a new account anytime!")
+  }
 }));
 
 // SUMMARY
