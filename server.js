4fa8416f99186cd0253128802abe2db74d83504b
Klaudia Wróblewska <kwroblewska0410@gmail.com>
2023-12-13 10:39:54 +0100

    delete folder

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ import dotenv from 'dotenv'; // Import dotenv for environment variables
 dotenv.config(); // Load environment variables from the .env file
 
 import userRoutes from './routes/userRoutes'; // Import custom user routes
-import { connectDB } from './config/db'; // Import database connection function (not used here)
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 const port = process.env.PORT; // Set the port number for the server
@@ -21,8 +20,23 @@ app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data
 
 app.use(userRoutes); // Use the user-controlled routes for user-related requests
 
-// Connection to the database through Mongoose
-connectDB();
+const connectDB = async () => {
+  try {
+    // Attempt to connect to the MongoDB database using the URL from the environment variables
+    // Mongoose Method: mongoose.connect()
+    // Description: This line of code serves the crucial purpose of connecting the Node.js application to the MongoDB database specified by the URL provided in the environment variable MONGO_URL. Once this connection is established, the application can perform various database operations, such as querying and modifying data in the MongoDB database. It's a critical step in setting up the database connection for the application to work with MongoDB.
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+
+    // If the connection is successful, log a message indicating that the MongoDB is connected
+    console.log(`Mongo DB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    // If an error occurs during the connection attempt, log the error message
+    console.log(error);
+
+    // Exit the Node.js process with an exit code of 1 to indicate an error
+    process.exit(1);
+  }
+};
 
 // Start the server and listen for incoming requests on the specified port
 app.listen(port, () => {
