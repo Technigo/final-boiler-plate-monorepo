e1d13c4024359a2a11557da88103024b2f470b76
Carolina Luna <kroluna@gmail.com>
2023-12-14 09:10:48 +0100

    starting up with the map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
 import { BrowserRouter, Routes } from "react-router-dom";
-import routes from "./routes/routes";
+// import routes from "./routes/routes";
 import "./App.css";
 
 export const App = () => {
   return (
     <>
       <BrowserRouter>
-        <main>
-          {/* <Routes>{routes}</Routes> */}
-          <Routes>{routes}</Routes>
-        </main>
+        <main>{/* <Routes>{routes}</Routes> */}</main>
       </BrowserRouter>
     </>
   );
