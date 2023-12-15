e488d55b8d140481155d3c1f318a470708339be2
Inna Kokic <inna@Innas-MacBook-Pro-2.local>
2023-12-15 22:33:01 +0100

    Styling

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,38 +1,4 @@
-// import { IoIosGlobe } from "react-icons/io";
-// import { Button } from "./Button";
-// import { Link } from "react-router-dom";
-// import { Translations } from "../Translations/Translations";
-// import { useTranslation } from "react-i18next"
-// import "./navbar.css"
-
-// export const NavBar = () => {
-//   const { t } = useTranslation()
-
-//   return (
-//     <div className="navbar">
-//       <div className="left-section">
-//         <IoIosGlobe color="white" style={{ fontSize: '30px' }} > </IoIosGlobe>
-//         <Translations />
-//       </div>
-//       <Link to="/">
-//       <img 
-//       className="site-logo"
-//        src="./playground-svgrepo-com.svg" alt="" />
-//       </Link>
-//       <div className="right-section">
-//         {/* <Link to="/">
-//         <a href="#">{t("Navbar.home")}</a>
-//         </Link> */}
-//         <Link to="/login">
-//           <Button className={"my-favorites"} btnText={t("Navbar.myPages")} />
-//         </Link>
-//         {/* <a href="#">Mina Sidor</a> */}
-//       </div>
-//     </div>
-//   );
-// };
-
-//Satte in ny kod för att bara visa "my pages" om man är inloggad. Behöver lösa hur man fixar den översatta texten..
+//Satte in ny kod för att bara visa "my pages" om man är inloggad. 
 
 import { IoIosGlobe } from "react-icons/io";
 import { Button } from "./Button";
