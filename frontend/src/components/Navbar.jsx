e5d5e36042f0462f1bbe43d0b2a88f0a5c88f59d
Lisa Dahlkar <lisa.dahlkar@gmail.com>
2023-12-17 19:40:26 +0100

    styling navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,16 +18,17 @@ console.log('Current language in Navbar:', currentLanguage); */}
 
     return (
         <div className="navbar">
-            <div className="nav-links">
-                <Link to="/">{t('HOME')}</Link>
-                <Link to="/aboutus">{t('ABOUT US')}</Link>
-                <a href="#">{t('MY PAGE')}</a>
-                <Link to="/articles">{t('ARTICLES')}</Link>
-
-
-                <Link to="/register" className="member">{t('BECOME A MEMBER')}</Link>
-                <LanguageSwitcher />
-
+            <div className="navlinks">
+                <div className="nav-links1">
+                    <Link to="/">{t('HOME')}</Link>
+                    <Link to="/aboutus">{t('ABOUT US')}</Link>
+                    <a href="#">{t('MY PAGE')}</a>
+                    <Link to="/articles">{t('ARTICLES')}</Link>
+                </div>
+                <div className="nav-links2">
+                    <Link to="/register" className="member">{t('BECOME A MEMBER')}</Link>
+                    <LanguageSwitcher />
+                </div>
             </div>
         </div >
     );
