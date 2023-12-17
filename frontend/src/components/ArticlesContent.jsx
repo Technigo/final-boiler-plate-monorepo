1b1e38ecd0a05ed667860f97ac86ecff2130dcb0
Lisa Dahlkar <lisa.dahlkar@gmail.com>
2023-12-17 19:36:20 +0100

    articles

--- frontend/src/components/ArticlesContent.jsx
+++ frontend/src/components/ArticlesContent.jsx
@@ -1,3 +1,4 @@
+// ArticlesContent.jsx
 import React, { useState } from "react";
 import data from "../data/articles.json";
 import "./css/articlescontent.css";
@@ -11,38 +12,45 @@ const ArticlesContent = () => {
 
     const handleArticleClick = (articleId) => {
         // Set the clicked article's id as the expanded article
-        setExpandedArticle(articleId === expandedArticle ? null : articleId);
+        setExpandedArticle((prev) => (prev === articleId ? null : articleId));
     };
 
     return (
         <div className="articles-container">
             <h2>Articles</h2>
-            <p>Discover a variety of articles about habits. From understanding the science behind habit formation to practical tips for positive changes, our collection offers valuable insights for cultivating a healthier and more productive lifestyle.</p>
-            <hr></hr>
+            <p>
+                Discover a variety of articles about habits. From understanding the science behind habit
+                formation to practical tips for positive changes, our collection offers valuable insights for
+                cultivating a healthier and more productive lifestyle.
+            </p>
+            <hr />
 
             {articles.map((article) => (
-
-                <div className="article-info">
+                <div key={article.id} className={`article-summary ${article.isReverse ? "reverse" : ""}`}>
                     <div
-                        key={article.id}
-                        className={`article-summary ${expandedArticle === article.id ? "expanded" : ""}`}
+                        className={`article-info ${expandedArticle === article.id ? "expanded" : ""}`}
                         onClick={() => handleArticleClick(article.id)}
                     >
-
                         <img
                             src={expandedArticle === article.id ? article.imageL : article.imageS}
                             alt={article.name}
-                            style={{ maxWidth: expandedArticle === article.id ? "600px" : "300px" }}
+                            style={{ maxWidth: expandedArticle === article.id ? "800px" : "300px" }}
                         />
-
-                        <h3>{article.name}</h3>
-                        <p>{expandedArticle === article.id ? article.date : article.info}</p>
-                        {expandedArticle === article.id && (
-                            <>
-                                <p>{article.text}</p>
-                                <p>{article.date}</p>
-                            </>
-                        )}
+                        <div className="article-text">
+                            <h3>{article.name}</h3>
+                            {expandedArticle === article.id ? (
+                                <>
+                                    <p className="date">{article.date}</p>
+                                    <p>{article.text}</p>
+                                    <hr />
+                                </>
+                            ) : (
+                                <>
+                                    <p className="article-info">{article.info}</p>
+                                    <p className="readmore">{article.readmore}</p>
+                                </>
+                            )}
+                        </div>
                     </div>
                 </div>
             ))}
