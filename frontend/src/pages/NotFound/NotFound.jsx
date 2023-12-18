1331feef87845a83c8f77e00361bf4eb9edb926e
Emmy Jansson <emmybengtsson@live.se>
2023-12-18 09:13:45 +0100

    deleted notFound Animation

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,14 +1,1 @@
-// Import the 'React' library.
-import React from "react";
-import lottie from "lottie-react";
-// import animationData from "./src/assets/Animation404.json";
 
-// Define the 'NotFound' functional component.
-export const NotFound = () => {
-  // Render a div element with a CSS class 'not-found' containing the Lottie animation.
-  return (
-    <div className="not-found">
-      <lottie animationData={Animation404} />
-    </div>
-  );
-};
