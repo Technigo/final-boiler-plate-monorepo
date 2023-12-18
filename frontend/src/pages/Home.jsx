7af03e741a496676006c37b2634d548ca8092c2f
Emmy Dieden <emmydieden@Emmys-MacBook-Pro.local>
2023-12-18 11:30:39 +0100

    deleted unnecessary files

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,6 @@ import { PromptForm } from '../components/PromptForm';
 import { NewRecipe } from '../components/newRecipe/NewRecipe';
 import { CollectionRecipes } from "../components/collectionRecipes/CollectionRecipes"
 import { recipeStore } from '../stores/recipeStore';
-import { TestOpenAI } from '../components/testOpenAI';
 
 export const Home = () => {
   const { newRecipe } = recipeStore()
@@ -14,7 +13,6 @@ export const Home = () => {
       <PromptForm />
       {newRecipe && <NewRecipe />} {/* Conditionally render NewRecipe only when newRecipe is truthy */}
       <CollectionRecipes />
-      <TestOpenAI/>
     </>
   );
 };
