7fa6bcbc2eaaf96a34c54899d76ae51d33d503ed
Lisa Dahlkar <lisa.dahlkar@gmail.com>
2023-12-19 16:42:38 +0100

    added components in backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,20 +1,16 @@
-// Import the necessary modules and functions
 import express from "express";
 import {
   registerUserController,
   loginUserController,
-} from "../controllers/userController"; // Import controller functions for user registration and login
+} from "../controllers/userController";
+import { validateRegistration, validateLogin } from "../middleware/validation";
 
-// Create an instance of the Express router
 const router = express.Router();
 
-// REGISTER ROUTE: Handle user registration
-router.post("/register", registerUserController); // When a POST request is made to /register, execute the registerUserController function
+// REGISTER ROUTE: Handle user registration with validation middleware
+router.post("/register", validateRegistration, registerUserController);
 
-// LOGIN ROUTE: Handle user login
-router.post("/login", loginUserController); // When a POST request is made to /login, execute the loginUserController function
+// LOGIN ROUTE: Handle user login with validation middleware
+router.post("/login", validateLogin, loginUserController);
 
-// Export the router for use in the main application
 export default router;
-
-// In summary, this file sets up routes using the Express router for user registration and login operations. It associates each route with the corresponding controller function. These routes define the API endpoints for handling user registration and login within the application.
--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,28 @@
 // Import necessary libraries and modules
-import express from "express"; // Import the Express.js framework
-import cors from "cors"; // Import the CORS middleware
-import dotenv from "dotenv"; // Import dotenv for environment variables
-dotenv.config(); // Load environment variables from the .env file
-import taskRoutes from "./routes/taskRoutes"; // Import custom task controlled-routes
-import userRoutes from "./routes/userRoutes"; // Import custom user routes
-import { connectDB } from "./config/db"; // Import database connection function (not used here)
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import taskRoutes from "./routes/taskRoutes";
+import userRoutes from "./routes/userRoutes";
+import habitRoutes from "./routes/habitRoutes"; // Import custom habit controlled-routes
+import { connectDB } from "./config/db";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-const port = process.env.PORT; // Set the port number for the server
-const app = express(); // Create an instance of the Express application
+const port = process.env.PORT;
+const app = express();
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors()); // Enable CORS (Cross-Origin Resource Sharing)
-app.use(express.json()); // Parse incoming JSON data
-app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Use the routes for handling API requests
-// ROUTES - These routes USE controller functions ;)
-app.use(taskRoutes); // Use the task-controlled routes for task-related requests
-app.use(userRoutes); // Use the user-controlled routes for user-related requests
+app.use(taskRoutes);
+app.use(userRoutes);
+app.use(habitRoutes); // Use the habit-controlled routes for habit-related requests
 
 // Connection to the database through Mongoose
 connectDB();
 
-// Start the server and listen for incoming requests on the specified port
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`); // Display a message when the server is successfully started
+  console.log(`Server running on http://localhost:${port}`);
 });
