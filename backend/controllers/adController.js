046158feb5e7d92b1497fe9606610581860e9832
mariateresepettersson <mariateresepettersson>
2023-12-20 10:02:37 +0100

    Changed controllers to handle file upload and Cloudinary config

--- backend/controllers/adController.js
+++ backend/controllers/adController.js
@@ -8,21 +8,21 @@ import { UserModel } from "../models/UserModel";
 // route: /getAllAds
 // access: Private
 export const getAllAdsController = asyncHandler(async (req, res) => {
-  const allAds = await AdModel.find();
+  const userStorage = req.user;
+  const allAds = await AdModel.find().populate("user", "username");
   res.status(200).json(allAds);
 });
 
 // desciption: Get Ads
-// route: /get
+// route: /getAds
 // access: Private
 export const getAdsController = asyncHandler(async (req, res) => {
-  // get the user and matchIt with the user from the db - remmeber that we are using the accessToken to do so :)
   const userStorage = req.user;
-  // Use the AdModel to find all ads associated with the logged-in user
-  await AdModel.find({ user: userStorage })
+  const ads = await AdModel.find({ user: userStorage })
     .sort("-createdAt")
-    .then((result) => res.json(result)) // Respond with the found ads in JSON format
-    .catch((err) => res.json(err)); // Handle any errors that occur during the operation
+    .populate("user", "username"); // Populate the user field
+
+  res.json(ads);
 });
 
 // desciption: POST Ad
@@ -31,97 +31,146 @@ export const getAdsController = asyncHandler(async (req, res) => {
 export const addAdController = asyncHandler(async (req, res) => {
   try {
     console.log("Request body:", req.body); // Log the entire request body
+    console.log("req.file", req.file);
     // Extract the ad data from the request body
-    const { image, title, description, product, quantity, unit, address, pickupTime } = req.body;
-    
+    const { title, description, product, quantity, unit, address, pickupTime } = req.body;
+
     // Extract the accessToken from the request header key "Authorization"
     const accessToken = req.header("Authorization"); // we are requesting the Authorization key from the headerObject
-    
+
     //Find the user that matches the accessToken stored in the db
-    const userFromStorage = await UserModel.findOne({accessToken: accessToken});
-   
+    const userFromStorage = await UserModel.findOne({ accessToken: accessToken });
+
+    if (!userFromStorage) {
+      return res.status(401).json({ message: "Unauthorized: User not found." });
+    }
+
+    if (!req.file) {
+      return res.status(400).json({ message: "No image file provided." });
+    }
+
+    let imageUrl, imageId;
+    try {
+      // Upload the image file to Cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path);
+      imageUrl = result.url;
+      imageId = result.public_id; // or use req.file.filename for filename
+
+    } catch (uploadError) {
+      console.error('Cloudinary Upload Error:', uploadError);
+      return res.status(500).json({ message: "Error uploading image to Cloudinary.", error: uploadError });
+    }
     // Define var to pass new AD
     //wait for the save() operation to complete before sending back the response
-    const newAd = await new AdModel({
-      image,
+    const newAd = new AdModel({
       title,
-      description, 
+      description,
       product,
       quantity,
       unit,
       address,
       pickupTime,
+      image: imageUrl,
+      imageId: imageId,
       user: userFromStorage,
-    }).save();
-    res.status(201).json(newAd);
+    });
+
+    const savedAd = await newAd.save();
+    res.json(savedAd);
   } catch (error) {
-    res.status(500).json(error);
+    console.error(error); // Log the detailed error
+    res.status(500).json({ message: "Internal server error", error });
   }
 });
 
-// desciption: PUT/PATCH a specific AD to mark it complete
-// route: /update/:id"
+
+// desciption: PUT/PATCH a specific AD
+// route: /update/:id
 // access: Private
 export const updateAdController = asyncHandler(async (req, res) => {
-  // Extract the ad ID from the request parameters
   const { id } = req.params;
-  console.log(id); // Log the ID to the console
-  // Use AdModel to find and update an Ad by its ID, marking it as sold
-  // Use AdModel to delete all ads in the database of that specific user
-  // Extract the accessToken from the request object, but it is not going to be from the req.body but, its going to be from the req.header
-  const accessToken = req.header("Authorization"); // we are requesting the Authorization key from the headerObject
-  // get the user and matchIt with the user from the db - remmeber that we are using the accessToken to do so :)
-  const userFromStorage = await UserModel.findOne({
-    accessToken: accessToken,
-  });
-  await AdModel.findByIdAndUpdate(
-    { _id: id },
-    { sold: true },
-    { user: userFromStorage }
-  )
-    .then((result) => res.json(result)) // Respond with the updated ad in JSON format
-    .catch((err) => res.json(err)); // Handle any errors that occur during the operation
+  const updateData = req.body; // This contains the fields to be updated
+
+  // Optionally, if you're updating the image, handle the image file upload and get the new image URL and ID
+  if (req.file) {
+    try {
+      // Upload the new image file to Cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path);
+      updateData.image = result.url;
+      updateData.imageId = result.public_id;
+    } catch (uploadError) {
+      console.error('Cloudinary Upload Error:', uploadError);
+      return res.status(500).json({ message: "Error uploading new image to Cloudinary.", error: uploadError });
+    }
+  }
+
+  // Make sure to check that the user making the update is the owner of the ad
+  const userFromStorage = await UserModel.findOne({ accessToken: req.header("Authorization") });
+  if (!userFromStorage) {
+    return res.status(401).json({ message: "Unauthorized: User not found." });
+  }
+
+  // Update the ad with the new data
+  AdModel.findByIdAndUpdate(id, updateData, { new: true }) // {new: true} will return the updated document
+    .then((updatedAd) => {
+      if (!updatedAd) {
+        return res.status(404).json({ message: "Ad not found." });
+      }
+      res.json(updatedAd);
+    })
+    .catch((err) => res.status(500).json({ message: "Error updating ad.", error: err }));
 });
 
 // desciption: DELETE all ads
 // route: /deleteAll
 // access: Private
 export const deleteAllAdsController = asyncHandler(async (req, res) => {
-  // Use AdModel to delete all ads in the database
-  // Extract the accessToken from the request object, but it is not going to be from the req.body but, its going to be from the req.header
-  const accessToken = req.header("Authorization"); // we are requesting the Authorization key from the headerObject
-  // get the user and matchIt with the user from the db - remmeber that we are using the accessToken to do so :)
-  const userFromStorage = await UserModel.findOne({
-    accessToken: accessToken,
+  const accessToken = req.header("Authorization");
+
+  const userFromStorage = await UserModel.findOne({ accessToken });
+  if (!userFromStorage) {
+    return res.status(401).json({ message: "Unauthorized: User not found." });
+  }
+
+  // Find all ads for the user
+  const ads = await AdModel.find({ user: userFromStorage });
+
+  // Iterate over all ads and delete associated images from Cloudinary
+  for (const ad of ads) {
+    await cloudinary.uploader.destroy(ad.imageId);
+  }
+
+  // After all images are deleted, delete the ads from the database
+  const result = await AdModel.deleteMany({ user: userFromStorage });
+  res.json({
+    message: "All ads and associated images deleted",
+    deletedCount: result.deletedCount,
   });
-  await AdModel.deleteMany({ user: userFromStorage })
-    .then((result) =>
-      res.json({
-        message: "All ads deleted",
-        deletedCount: result.deletedCount,
-      })
-    ) // Respond with a success message and the count of deleted ads
-    .catch((err) => res.status(500).json(err)); // Handle any errors that occur during the operation
 });
 
 // desciption: DELETE AD by its ID
 // route: /delete/:id
 // access: Private
 export const deleteSpecificAdController = asyncHandler(async (req, res) => {
-  // Extract the ad ID from the request parameters
   const { id } = req.params;
-  // Use AdModel to find and delete a ad by its ID
-  await AdModel.findByIdAndDelete(id)
-    .then((result) => {
-      if (result) {
-        res.json({
-          message: "Ad deleted successfully",
-          deletedAd: result,
-        }); // Respond with a success message and the deleted Ad
-      } else {
-        res.status(404).json({ message: "Ad not found" }); // Respond with a 404 error if the AD is not found
-      }
-    })
-    .catch((err) => res.status(500).json(err)); // Handle any errors that occur during the operation
-});
 
+  const ad = await AdModel.findById(id);
+  if (!ad) {
+    return res.status(404).json({ message: "Ad not found" });
+  }
+
+  try {
+    // Delete the image from Cloudinary using the imageId
+    await cloudinary.uploader.destroy(ad.imageId);
+
+    // Then delete the ad from the database
+    const result = await AdModel.findByIdAndDelete(id);
+    res.json({
+      message: "Ad and associated image deleted successfully",
+      deletedAd: result,
+    });
+  } catch (err) {
+    console.error('Error during ad deletion:', err);
+    res.status(500).json({ message: "Failed to delete ad and/or image", error: err });
+  }
+});
