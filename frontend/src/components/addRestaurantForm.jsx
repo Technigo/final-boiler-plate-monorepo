f4dddf83f72f0a34bbc6f73bc9a15f859ad0d265
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2023-12-20 10:50:52 +0100

    -m

--- frontend/src/components/addRestaurantForm.jsx
+++ frontend/src/components/addRestaurantForm.jsx
@@ -1,4 +1,28 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import Navbar from './navbar';
+import Footer from './footer';
+
+const PageContainer = styled.div`
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #FBB7C0; /* Your chosen color */
+  min-height: 100vh; /* Make sure it covers the full height of the viewport */
+`;
+
+const OccasionButton = styled.button`
+  margin: 0.5em;
+  padding: 0.5em 1em;
+  border: none;
+  background-color: ${({ selected }) => selected ? '#a5d6a7' : '#efefef'};
+  font-size: 16px;
+  cursor: pointer;
+`;
+
+const OccasionSelectorContainer = styled.div`
+  font-size: 20px;
+  font-family: Arial, sans-serif;
+`;
 
 const AddRestaurantForm = () => {
     const [formData, setFormData] = useState({
@@ -87,52 +111,57 @@ const AddRestaurantForm = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <h1>Add a New Restaurant</h1>
-            {/* Input fields for each property in your schema */}
-            {/* ... (other fields) ... */}
-
-            {/* Occasion (checkboxes) */}
-            <div>
-                <p>Occasion:</p>
-                {occasionOptions.map((option) => (
-                    <label key={option}>
-                        {option}
-                        <input
-                            type="checkbox"
-                            name="occasion"
-                            value={option}
-                            checked={formData.occasion.includes(option)}
-                            onChange={handleChange}
-                        />
-                    </label>
-                ))}
-            </div>
-            <br />
-            
-            {/* Mood (checkboxes) */}
-            <div>
-                <p>Mood:</p>
-                {moodOptions.map((option) => (
-                    <label key={option}>
-                        {option}
-                        <input
-                            type="checkbox"
-                            name="mood"
-                            value={option}
-                            checked={formData.mood.includes(option)}
-                            onChange={handleChange}
-                        />
-                    </label>
-                ))}
-            </div>
-            <br />
-            
-            {/* Add fields for description and URL */}
-            {/* ... (description and URL fields) ... */}
-            
-            <button type="submit">Submit</button>
-        </form>
+        <PageContainer>
+    
+            <Navbar />
+            <form onSubmit={handleSubmit}>
+                <h1>Add a New Restaurant</h1>
+                {/* Input fields for each property in your schema */}
+                {/* ... (other fields) ... */}
+    
+                {/* Occasion (checkboxes) */}
+                <div>
+                    <p>Occasion:</p>
+                    {occasionOptions.map((option) => (
+                        <label key={option}>
+                            {option}
+                            <input
+                                type="checkbox"
+                                name="occasion"
+                                value={option}
+                                checked={formData.occasion.includes(option)}
+                                onChange={handleChange}
+                            />
+                        </label>
+                    ))}
+                </div>
+                <br />
+                
+                {/* Mood (checkboxes) */}
+                <div>
+                    <p>Mood:</p>
+                    {moodOptions.map((option) => (
+                        <label key={option}>
+                            {option}
+                            <input
+                                type="checkbox"
+                                name="mood"
+                                value={option}
+                                checked={formData.mood.includes(option)}
+                                onChange={handleChange}
+                            />
+                        </label>
+                    ))}
+                </div>
+                <br />
+                
+                {/* Add fields for description and URL */}
+                {/* ... (description and URL fields) ... */}
+                
+                <button type="submit">Submit</button>
+            </form>
+            <Footer />
+        </PageContainer>
     );
 };
 
