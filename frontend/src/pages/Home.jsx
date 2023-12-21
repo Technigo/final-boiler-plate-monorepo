abac2657f8f8ffd3d4addd521e44d767c8cd21da
isaackamran <isaackamran@hotmail.com>
2023-12-21 14:52:40 +0100

    added animation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,20 @@
+import { Link } from "react-router-dom";
+import Lottie from "lottie-react"
+import HomePicture from "../HomePicture2.json"
+
 export const Home = () => {
 
 
-  return <div>Welcome Page</div>;
-  
+  return (
+    <>
+      <h1>Welcome to Piggy Back</h1>
+      <Lottie animationData={HomePicture} />
+      <h2>Your simple and safe carpool near you</h2>
+      <p>
+        <Link to="/login">Login</Link>
+        
+        <Link to="/register">Register</Link>
+      </p>
+    </>
+  );
 };
