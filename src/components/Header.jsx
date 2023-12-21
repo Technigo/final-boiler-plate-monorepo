9dab24887de743979ae3f81fa9ef332285a0f5d2
Frida <139373483+FridaKristian@users.noreply.github.com>
2023-12-21 10:44:36 +0100

    Added a login-button to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,28 @@
 import { Link } from 'react-router-dom'
+import { userStore } from '../store/userStore'
+import userIcon from '/user.svg'
+import './header.css'
 
 export const Header = () => {
-	return <header>{<Link to="/">HOME</Link>}</header>
+	const isLoggedIn = userStore.getState().isLoggedIn
+
+	return (
+		<header className="the-header">
+			<div>{<Link to="/">HOME</Link>}</div>
+			<div>
+				{isLoggedIn ? (
+					<img src={userIcon} />
+				) : (
+					<>
+						<Link to="/login">
+							<img
+								src={userIcon}
+								className="login-icon"
+							/>
+						</Link>
+					</>
+				)}
+			</div>
+		</header>
+	)
 }
