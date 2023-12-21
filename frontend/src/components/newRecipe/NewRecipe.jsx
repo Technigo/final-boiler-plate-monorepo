f9135437804552472eea798d6645c2e399d2f1e7
Idah Collin <idah_collin@hotmail.com>
2023-12-21 09:18:59 +0100

    Test to hide env

--- frontend/src/components/newRecipe/NewRecipe.jsx
+++ frontend/src/components/newRecipe/NewRecipe.jsx
@@ -3,24 +3,28 @@ import { recipeStore } from "../../stores/recipeStore";
 
 export const NewRecipe = () => {
   const { newRecipe } = recipeStore();
+  // console.log(newRecipe)
+  // console.log(newRecipe.recipe.description)
+  // console.log(newRecipe.recipe.ingredients)
+  // console.log(newRecipe.recipe.instructions)
 
   return (
     <div>
       <h3>Your recipe:</h3>
-      <h2>{newRecipe.title}</h2>
-      <p><strong>Description:</strong> {newRecipe.description}</p>
-       {/* Iterate over the ingredients object */}
-       <p><strong>Ingredients:</strong></p>
+      <h2>{newRecipe.recipe.title}</h2>
+      <p><strong>Description:</strong> {newRecipe.recipe.description}</p>
+      {/* Iterate over the ingredients object */}
+      <p><strong>Ingredients:</strong></p>
       {/* <ul>
-        {Object.entries(newRecipe.ingredients).map(([ingredient, quantity], i) => (
+        {Object.entries(newRecipe.recipe.ingredients).map(([ingredient, quantity], i) => (
           <li key={i}>{`${ingredient}: ${quantity}`}</li>
         ))}
       </ul> */}
-    
+
       {/* <div>
         <p><strong>Instructions:</strong></p>
         <ol>
-          {newRecipe.instructions.map((instruction, index) => (
+          {newRecipe.recipe.instructions.map((instruction, index) => (
             <li key={index}>{instruction}</li>
           ))}
         </ol>
