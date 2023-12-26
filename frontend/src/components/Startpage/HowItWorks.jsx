0df214f6686811f5825bfc5e9a020e7f4eb18530
Linnea Johansson <Linneajoh@gmail.com>
2023-12-26 13:26:51 +0100

    how it works

--- frontend/src/components/Startpage/HowItWorks.jsx
+++ frontend/src/components/Startpage/HowItWorks.jsx
@@ -7,12 +7,26 @@ const HowItWorks = () => {
     const { t } = useTranslation();
 
     return (
-        <div>
+        <div className="how-it-works-container">
             <h1>{t("How it works")}</h1>
-            <p>{t("Not finnished yet")}</p>
+            <h2>{t("> You choose your habits")}</h2>
+            <h2>{t("> Track your daily progress")}</h2>
+            <h2>{t("> See how your longest streak last")}</h2>
+            <h2>{t("> See how many weeks you have finnished")}</h2>
+            <h2>{t("> Up to 10 habits")}</h2>
         </div>
 
     );
 };
 
 export default HowItWorks; 
+
+
+
+
+
+
+
+
+
+
