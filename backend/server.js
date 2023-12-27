ed67603c58c0e8df35d505ce0ce79e803b80728a
So Youn Choi <manghatuhyo@gmail.com>
2023-12-27 13:19:05 +0100

    instead of using seperate config db js unless i rly need this. so tried to merge this into server js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 // Import necessary libraries and modules
 import express from "express"; // Import the Express.js framework
 import cors from "cors"; // Import the CORS middleware
-import dotenv from "dotenv"; // Import dotenv for environment variables
+import dotenv from "dotenv"; // Import dotenv for environment variables from a .env file
+import mongoose from "mongoose"; // Import mongoose library to work with mongodb
+
 dotenv.config(); // Load environment variables from the .env file
 import taskRoutes from "./routes/taskRoutes"; // Import custom task controlled-routes
 import userRoutes from "./routes/userRoutes"; // Import custom user routes
-import { connectDB } from "./config/db"; // Import database connection function (not used here)
+
+// import { connectDB } from "./config/db"; // Import database connection function (not used here)
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 const port = process.env.PORT; // Set the port number for the server
@@ -15,6 +18,24 @@ const app = express(); // Create an instance of the Express application
 app.use(cors()); // Enable CORS (Cross-Origin Resource Sharing)
 app.use(express.json()); // Parse incoming JSON data
 app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data
+
+// Define an asynchronous function 'connectDB' to connect to the MongoDB database
+const connectDB = async () => {
+  try {
+    // Attempt to connect to the MongoDB database using the URL from the environment variables
+    // Mongoose Method: mongoose.connect()
+    // Description: This line of code serves the crucial purpose of connecting the Node.js application to the MongoDB database specified by the URL provided in the environment variable MONGO_URL. Once this connection is established, the application can perform various database operations, such as querying and modifying data in the MongoDB database. It's a critical step in setting up the database connection for the application to work with MongoDB.
+    const conn = await mongoose.connect(process.env.MONGO_URL)
+
+    // If the connection is successful, log a message indicating that the MongoDB is connected
+    console.log(`MongoDB Connected: ${conn.connection.host}`) 
+  } catch (error) {
+    // If an error occurs during the connection attempt, log the error message
+    console.log(error)
+    // Exit the Node.js process with an exit code of 1 to indicate an error
+    process.exit(1)
+  }
+}
 
 // Use the routes for handling API requests
 // ROUTES - These routes USE controller functions ;)
