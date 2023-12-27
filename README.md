9ebd6f716884337c5f6ee5501a6bda8d0caa32de
So Youn Choi <manghatuhyo@gmail.com>
2023-12-27 11:09:21 +0100

    not wanting seperate component, moved header to app jsx. currently having problem with autoplay. it is not letting user to autoplay, but some version that stayed my chrome tab is autoplaying. so im confused and wanted to make this autoplay if i can. still no clue...

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # https://www.youtube.com/watch?v=BjOq9SEDzKY
+
+# "set up backend first" - by code coach
+but im working with frontend currently.
+when user start to 'move'
+api is supposed to record users place,
+so, and then, having need to set up backend
+but i think there should be reason, to focus on backend first, and i want to feel that
+so lets hope that ill be done the basic frontend thing asap,
+and get into, the real thing, called backend.
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
 import { useEffect } from "react";
-import Header from "./components/Header"
+// import Header from "./components/Header"
+import YouTube from "react-youtube"
 
 export const App = () => {
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1,
+      // mute: 1, // i added this to test autoplay. but enabling this even not giving mute the video
+    },
+  }
+
+  const videoUrl = "https://www.youtube.com/watch?v=BjOq9SEDzKY"
+
+  // event handler for when the video ends
+  const onEnd = (event) => {
+    // do something when the video ends (if needed)
+    console.log("video ended:", event)
+  }
+
   useEffect(() => {
     // document.body.style.textAlign = 'center'
     document.body.style.fontFamily = 'Roboto, sans-serif'
@@ -26,7 +44,11 @@ export const App = () => {
   }, []) // the empty dependency array ensures this effect runs only once when the component mounts
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
+      <header>
+        <h1>you are alone in the dark ...</h1>
+        <YouTube videoId={videoUrl.split("v=")[1]} opts={opts} onEnd={onEnd} />
+      </header>
       <div>
         "you are alone in the dark ... "
       </div>
