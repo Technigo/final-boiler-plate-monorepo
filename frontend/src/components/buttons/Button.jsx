b308ee7575c5713a7152ff55980cd2e182d56ce8
JuliaHolm <juliat.holm@gmail.com>
2023-12-30 23:11:57 +0100

    deleted all files

--- frontend/src/components/buttons/Button.jsx
+++
@@ -1,9 +0,0 @@
-import "./Button.css";
-
-export const Button = ({ className, onClick, btnText, ariaLabel }) => {
-  return (
-    <button className={className} onClick={onClick} aria-label={ariaLabel}>
-      {btnText}
-    </button>
-  );
-};
