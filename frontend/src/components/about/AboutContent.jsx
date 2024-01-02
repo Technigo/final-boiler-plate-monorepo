b0d5d78d221750c4b38b1e6117569c7a1a69bfc8
Idah Collin <idah_collin@hotmail.com>
2024-01-02 19:55:58 +0100

    Little styling for About-page

--- frontend/src/components/about/AboutContent.jsx
+++ frontend/src/components/about/AboutContent.jsx
@@ -8,11 +8,11 @@ export const AboutContent = () => {
             <div className="card">
                 <div className="card-content">
                     <img src="https://idah-collin-portfolio.netlify.app/assets/profilbildidah.jpeg" alt="Photo of Idah Collin" />
-                    <h2>Idah Collin</h2>
+                    <h3>Idah Collin</h3>
                     <div className="contact-idah">
-                        <a href="https://www.linkedin.com/in/idah-collin"><FaLinkedin/></a>
-                        <a href="https://github.com/IdahCollin"><FaGithub/></a>
-                       </div>
+                        <a href="https://www.linkedin.com/in/idah-collin"><FaLinkedin /></a>
+                        <a href="https://github.com/IdahCollin"><FaGithub /></a>
+                    </div>
                 </div>
             </div>
 
@@ -20,13 +20,27 @@ export const AboutContent = () => {
             <div className="card">
                 <div className="card-content">
                     <img src="https://emmy-dieden-portfolio.netlify.app/assets/gifs/Profilbild-min.JPG" alt="Photo of Emmy Dieden" />
-                    <h2>Emmy Dieden</h2>
-                    <div className="contact-Emmy">
-                    <a href="https://www.linkedin.com/in/emmy-dieden-774574283/"><FaLinkedin/></a>
-                    <a href="https://github.com/EmmyDieden"><FaGithub/></a>
-                      </div>
+                    <h3>Emmy Dieden</h3>
+                    <div className="contact-emmy">
+                        <a href="https://www.linkedin.com/in/emmy-dieden-774574283/"><FaLinkedin /></a>
+                        <a href="https://github.com/EmmyDieden"><FaGithub /></a>
+                    </div>
                 </div>
             </div>
+            <div className="about-vision">Our vision.... Info info text test text test Info info text test text test Info info text test text test Info info text test text test varför kan inte denna och nästa div ligga utanför card-container diven?</div>
+            <div className="about-tech">
+                <h3>Tech</h3>
+                <ul>
+                    <li>React</li>
+                    <li>JavaScript</li>
+                    <li>Zustand</li>
+                    <li>OpenAI</li>
+                    <li>MongoDB</li>
+                    <li>Fyll på</li>
+                </ul>
+                <h4>More information about the project: Länk till Read-me på GitHub?</h4>
+            </div>
         </div>
+
     )
 }
