3d96953e11642fed68c309be0e985ba1ec151755
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2024-01-03 09:53:15 +0100

    to maing

--- frontend/src/components/about.jsx
+++ frontend/src/components/about.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './navbar';
 import Footer from './footer';
 import styled from 'styled-components';
+import alexandra from '../assets/alexandra.jpg'
 
 
 const AboutWrapper = styled.div`
@@ -35,6 +36,8 @@ const TeamMember = styled.div`
 const TeamMemberPhoto = styled.img`
   max-width: 100%;
   border-radius: 50%;
+  height: 120px;
+  width: 120px;
 `;
 
 const TeamMemberName = styled.h3`
@@ -62,7 +65,8 @@ const AboutPage = () => {
         <IntroParagraph>
           {/* Your intro paragraph content */}
           We are three coders who teamed up during our final sprint of the 
-          <a href="https://www.technigo.io/">Technigo</a> web developer bootcamp to bring you the Foodie moodie app. 
+          <a href="https://www.technigo.io/" target="_blank" rel="noopener noreferrer">Technigo</a>
+web developer bootcamp to bring you the Foodie moodie app. 
         </IntroParagraph>
         <TeamSection>
           <TeamMember>
@@ -71,7 +75,7 @@ const AboutPage = () => {
             <TeamMemberDescription>
               Brief description of Team Member 1 goes here.
             </TeamMemberDescription>
-            <PortfolioLink href="portfolio1-link">Portfolio</PortfolioLink>
+            <PortfolioLink href="portfolio1-link" target="_blank" rel="noopener noreferrer">Portfolio</PortfolioLink>
           </TeamMember>
 
           <TeamMember>
@@ -80,16 +84,16 @@ const AboutPage = () => {
           <TeamMemberDescription>
             Brief description of Team Member 2 goes here.
           </TeamMemberDescription>
-          <PortfolioLink href="portfolio2-link">Portfolio</PortfolioLink>
+          <PortfolioLink href="portfolio2-link" target="_blank" rel="noopener noreferrer">Portfolio</PortfolioLink>
         </TeamMember>
 
         <TeamMember>
-          <TeamMemberPhoto src="placeholder3.jpg" alt="Team Member 3" />
+        <TeamMemberPhoto src={alexandra} alt="Alexandra Meija" />
           <TeamMemberName>Alexandra Meija</TeamMemberName>
           <TeamMemberDescription>
          For me a foodie is someone who really loves food, and not just the high- end fancy pants restaurants, but just plain good food, either it is an amazing fishcake you buy for 10 bahts from a street vendor or having a seven course meal at Ekstedt. And for me the experience is enhanced when you find that perfect spot that suits you and the people you are with, that you can hold a conversation and that it's cozy. I always look for dogfriendly places so I can take with me my furrycompanion. I have a lot of favorites, but if I had to pick one place it's Bar Doma, it works for everything, and yes, it's dog friendly. An extra neat feature is the dessertcart which gets rolled out after every meal.
           </TeamMemberDescription>
-          <PortfolioLink href="portfolio3-link">Portfolio</PortfolioLink>
+          <PortfolioLink href="https://alexandrameijaportfolio.netlify.app" target="_blank" rel="noopener noreferrer">Portfolio</PortfolioLink>
         </TeamMember>
         </TeamSection>
       </AboutWrapper>
