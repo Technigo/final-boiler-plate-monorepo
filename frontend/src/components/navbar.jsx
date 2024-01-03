fb25ab2c485fe633dc5a0df30b76088b5c1dc768
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2024-01-03 13:30:14 +0100

    -m changes in navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -4,12 +4,13 @@ import { Link } from 'react-router-dom';
 
 
 const NavbarContainer = styled.nav`
-  background-color: #FFCCD5; 
-  color: #504746;
+  background-color: #01999a;
+  color: #66feff;
+  border-radius: 5px;
   display: flex;
   justify-content: space-between;
-  padding: 1rem;
-`;
+  padding: 5px;
+  `;
 
 const NavbarNav = styled.ul`
   list-style: none;
@@ -19,17 +20,53 @@ const NavbarNav = styled.ul`
 `;
 
 const NavItem = styled.li`
-  margin: 0 1rem;
+  margin: 2px;
+  display: none;
+  @media ${tablet} {
+    color: #66feff;
+    font-size: 45px;
+    font-family: "Mercusuar";
+    display: inline-block;
+    justify-content: flex-end;
+    margin-right: 15px;
+    text-decoration: none;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+  @media ${desktop} {
+    color: #66feff;
+    font-size: 45px;
+    font-family: "Mercusuar";
+    text-decoration: none;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 const NavbarLink = styled(Link)`
-  color: #800F2F;
-  font-size: 16px; 
-  font-family: Montserrat, sans-serif; 
+display: none;
+@media ${tablet} {
+  color: #66feff;
+  font-size: 45px;
+  font-family: "Mercusuar";
+  display: inline-block;
+  justify-content: space-around;
   text-decoration: none;
   &:hover {
     text-decoration: underline;
   }
+}
+@media ${tablet} {
+  color: #66feff;
+  font-size: 45px;
+  font-family: "Mercusuar";
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+}
 `;
 
 const Navbar = () => {
