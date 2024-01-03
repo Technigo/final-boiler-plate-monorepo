6132283cb2f23de7269eaffddf72905d2a0fafbf
mariateresepettersson <mariateresepettersson>
2024-01-03 09:55:28 +0100

    Importing the controller

--- backend/routes/adRoutes.js
+++ backend/routes/adRoutes.js
@@ -6,6 +6,7 @@ import {
     getAllAdsController,
     getAdsController,
     getAdByIdController,
+    getAdsByUserIdController,
     updateAdController,
     deleteAllAdsController,
     deleteSpecificAdController,
