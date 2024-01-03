09f1071068f62a18a8bf091916e79bddd8605de4
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2024-01-03 10:40:22 +0100

    -m

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import AboutPage from './components/about';
-import MoodSelector from './components/mood';
-import OccasionSelector from './components/occasion';
-import ResultsComponent from './components/result';
-import Suggestion from './components/suggestion';
-import AddRestaurantForm from './components/addRestaurantForm';
+import React from "react";
+import GlobalFonts from "./fonts/font";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Wrapper from "./components/wrapper";
+import Home from "./components/Home";
+import AboutPage from "./components/about";
+import MoodSelector from "./components/mood";
+import OccasionSelector from "./components/occasion";
+import ResultsComponent from "./components/result";
+import AddRestaurantForm from "./components/addRestaurantForm";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body { 
+  font-family: 'Mercusaur';
+  font-family: 'JosefinSans';
+}`;
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/occasion" element={<OccasionSelector />} />
-        <Route path="/mood" element={<MoodSelector />} />
-        <Route path="/result" element={<ResultsComponent />} /> 
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/addrestaurant" element={<AddRestaurantForm />} />
-        <Route path="/suggestion" element={<Suggestion />} />
-      </Routes>
+       <Wrapper>
+        <GlobalFonts />
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/occasion" element={<OccasionSelector />} />
+          <Route path="/mood" element={<MoodSelector />} />
+          <Route path="/result" element={<ResultsComponent />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/addrestaurant" element={<AddRestaurantForm />} />
+        </Routes>
+      </Wrapper>
     </BrowserRouter>
   );
 };
