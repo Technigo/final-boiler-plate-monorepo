dc99dcd82c3993435484d367c0c55332b7de3ddf
andrkavr <72137459+andrkavr@users.noreply.github.com>
2024-01-03 14:50:18 +0100

    updated navbar display depending on login status

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,8 +2,10 @@ import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { LoginBtn } from "./LoginBtn";
 import { LogoutBtn } from "./LogoutBtn";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const Navbar = () => {
+  const { isAuthenticated } = useAuth0();
   const navigate = useNavigate();
   const [openMobileNav, setOpenMobileNav] = useState(false);
 
@@ -26,37 +28,64 @@ export const Navbar = () => {
     { linkName: "My Account", linkRoute: "/account" },
   ];
 
-  const renderMenuItems = () => (
-    <ul className="bg-gray-100 absolute top-15 w-full mt-4 left-0 p-2 space-y-2">
-      {navlinks.map((link) => (
-        <li key={link.linkName}>
-          <Link to={link.linkRoute}>{link.linkName}</Link>
-        </li>
-      ))}
-    </ul>
-  );
+  const renderMenuItems = () =>
+    isAuthenticated ? (
+      <ul className="bg-gray-100 absolute top-15 w-full mt-4 left-0 p-2 space-y-2">
+        {navlinksLoggedIn.map((link) => (
+          <li key={link.linkName}>
+            <Link to={link.linkRoute}>{link.linkName}</Link>
+          </li>
+        ))}
+      </ul>
+    ) : (
+      <ul className="bg-gray-100 absolute top-15 w-full mt-4 left-0 p-2 space-y-2">
+        {navlinks.map((link) => (
+          <li key={link.linkName}>
+            <Link to={link.linkRoute}>{link.linkName}</Link>
+          </li>
+        ))}
+      </ul>
+    );
 
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex items-center justify-between">
         <div
           onClick={() => navigate("/")}
-          className="text-white font-bold text-xl cursor-pointer">
+          className="text-white font-bold text-xl cursor-pointer"
+        >
           Piggyback
         </div>
-        <ul className="hidden md:flex space-x-4">
-          {navlinks.map((link) => (
-            <li className="text-white pt-2" key={link.linkName}>
-              <Link to={link.linkRoute}>{link.linkName}</Link>
+        {!isAuthenticated && (
+          <ul className="hidden md:flex space-x-4">
+            {navlinks.map((link) => (
+              <li className="text-white pt-2" key={link.linkName}>
+                <Link to={link.linkRoute}>{link.linkName}</Link>
+              </li>
+            ))}
+            <li>
+              <LoginBtn />
             </li>
-          ))}
-          <li>
-            <LoginBtn />
-          </li>
-          <li>
-            <LogoutBtn />
-          </li>
-        </ul>
+            <li>
+              <LogoutBtn />
+            </li>
+          </ul>
+        )}
+        {isAuthenticated && (
+          <ul className="hidden md:flex space-x-4">
+            {navlinksLoggedIn.map((link) => (
+              <li className="text-white pt-2" key={link.linkName}>
+                <Link to={link.linkRoute}>{link.linkName}</Link>
+              </li>
+            ))}
+            <li>
+              <LoginBtn />
+            </li>
+            <li>
+              <LogoutBtn />
+            </li>
+          </ul>
+        )}
 
         {/* Add a responsive menu button for smaller screens */}
         <div className="md:hidden">
--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -4,78 +4,84 @@ import { useState, useEffect } from "react";
 export const UserProfile = () => {
   const { user, isAuthenticated, isLoading, getAccessTokenSilently } =
     useAuth0();
-  const [userMetadata, setUserMetadata] = useState(null);
-  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  // const [userMetadata, setUserMetadata] = useState(null);
+  // const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const vite_backup = import.meta.env.VITE_BACKUP_API;
 
   const [username, setUsername] = useState(null);
   const [email, setEmail] = useState(null);
 
+  //#REGION USER_METADATA
   // console.log(useAuth0());
+  //Get user metadata if it exists
+  // useEffect(() => {
+  //   const getUserMetadata = async () => {
+  //     try {
+  //       const accessToken = await getAccessTokenSilently({
+  //         authorizationParams: {
+  //           audience: `https://${domain}/api/v2/`,
+  //           scope: "read:current_user",
+  //         },
+  //       });
+
+  //       const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+
+  //       const metadataResponse = await fetch(userDetailsByIdUrl, {
+  //         headers: {
+  //           Authorization: `Bearer ${accessToken}`,
+  //         },
+  //       });
+
+  //       const { user_metadata } = await metadataResponse.json();
+
+  //       setUserMetadata(user_metadata);
+  //     } catch (e) {
+  //       console.log(e.message);
+  //     }
+  //   };
+
+  //   getUserMetadata();
+  // }, [getAccessTokenSilently, user?.sub]);
+
+  //#ENDREGION
+
   useEffect(() => {
-    const getUserMetadata = async () => {
-      try {
-        const accessToken = await getAccessTokenSilently({
-          authorizationParams: {
-            audience: `https://${domain}/api/v2/`,
-            scope: "read:current_user",
-          },
-        });
-
-        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
-
-        const metadataResponse = await fetch(userDetailsByIdUrl, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-
-        const { user_metadata } = await metadataResponse.json();
-
-        setUserMetadata(user_metadata);
-      } catch (e) {
-        console.log(e.message);
-      }
-    };
-
     const getUserDataFromMongo = async () => {
+      console.log(`${vite_backup}/user/${user.sub}`);
       try {
-        fetch(`${VITE_BACKUP_API}/user/${JSON.stringify(user)}`)
+        await fetch(`${vite_backup}/user/${user.sub}`)
           .then((res) => res.json())
-          .then((data) => console.log(data));
+          .then((data) => {
+            setUsername(data.username);
+            setEmail(data.email);
+          });
       } catch (error) {
         console.log(error);
       }
     };
 
-    // const apiTest = () => {
-    //   fetch(`https://${domain}/api/v2/users/`)
-    //     .then((res) => res.json())
-    //     .then((data) => console.log(data));
-    // };
-    // apiTest();
+    getUserDataFromMongo();
+  }, []);
 
-    getUserMetadata();
-  }, [getAccessTokenSilently, user?.sub]);
-
+  console.log("User: " + JSON.stringify(user));
+  console.log("Authenticated: " + isAuthenticated);
   if (isLoading) {
     return <div>Loading ...</div>;
   }
 
-  console.log("User: " + JSON.stringify(user));
-  console.log("Authenticated: " + isAuthenticated);
-
   return (
     isAuthenticated && (
       <div className="text-black">
-        <img src={user.picture} alt={user.name} />
+        <h1 className="text-black text-2xl">Welcome {username}</h1>
+        {/* <img src={user.picture} alt={user.name} />
         <h2 className="text-black">{user.nickname}</h2>
-        <p className="text-black">{user.email}</p>
-        <h3>User Metadata</h3>
+        <p className="text-black">{user.email}</p> */}
+        {/* <h3>User Metadata</h3>
         {userMetadata ? (
           <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
         ) : (
           "No user metadata defined"
-        )}
+        )} */}
       </div>
     )
   );
