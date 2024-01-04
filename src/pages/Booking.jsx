67087ff86b4770bfefdbf6cea4340a9c371a5040
Frida <frida@Educuratios-iMac.local>
2024-01-04 20:57:17 +0100

    Clickable seats where the row and seat number are stored in useState when clicked and the button is changed to 'selected'. When clicked again the seat is removed from the useState and the 'selected'-class is removes

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,24 +1,76 @@
+import { useState, useEffect } from 'react'
 import data from '../showTime.json'
 
 import './Booking.css'
 
+const compareArrays = (arrayOne, arrayTwo) => {
+	let isSame = true
+
+	if (arrayOne.length !== arrayTwo.length) return false
+	arrayOne.map((element, index) => isSame = isSame && (element === arrayTwo[index]))
+
+	return (isSame)
+}
+
 export const Booking = () => {
-	// console.log(data)
+	const [ selectedSeats, setSelectedSeats ] = useState([])
+
 	const thisShowTime = data[1]
+	const movieTitle = thisShowTime.movieTitle
 	const seatInfo = thisShowTime.seats
-	
+
+	const handleSeatClick = (event, row, seat) => {
+		const newSelection = [row, seat]
+
+		const removeSelected = (event) => {
+			console.log('REMOVE')
+			event.target.classList.remove('selected')
+			let filteredArray = selectedSeats.filter(item => !compareArrays(item, newSelection))
+			setSelectedSeats(filteredArray)
+			console.log('filtered', filteredArray)
+		}
+
+		const addSelected = (event) => {
+			console.log('ADD')
+			event.target.classList.add('selected')
+			setSelectedSeats((selectedSeats) => [...selectedSeats, newSelection])
+		}
+
+		if (event.target.classList.contains('booked')) return null
+		if (selectedSeats !== null && selectedSeats.length > 0) {
+			let existsAlready = false
+			selectedSeats.map((seats) => {
+				compareArrays(seats, newSelection) ? existsAlready = !existsAlready : false
+			})
+			existsAlready ? removeSelected(event) : addSelected(event)
+		} else {
+			setSelectedSeats([newSelection])
+			event.target.classList.add('selected')
+		}
+	}
+
+	useEffect(() => {
+		console.log(selectedSeats)
+	},[selectedSeats])
 	
 	return (
 		<>
 			<h2>BOOKING</h2>
+			<h3>{movieTitle}</h3>
 			<section className="cinema-container">
 				<div className="the-screen">Screen</div>
 				<div className="seat-container">
 					{seatInfo.map(row => (
 						<div className="the-rows" key={row.rowIndex}>
 							<label>{row.rowIndex}</label>
-							{row.seats.map(seat => (
-								seat.booked ? <div className="seat booked" key={seat.bookingID}></div> : <div className="seat" key={seat.bookingID}></div>
+							{row.seats.map((seat, index) => (
+								<div 
+									className={`seat ${seat.booked ? "booked" : ""}`} 
+									key={index}
+									onClick={(event) => {
+											handleSeatClick(event, row.rowIndex, index)
+										}}>
+								</div>
 							))}
 						</div>
 					))}
