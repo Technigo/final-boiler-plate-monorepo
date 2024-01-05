7f8a78b860f59bbb83172960d48af571894afb6a
Idah Collin <idah_collin@hotmail.com>
2024-01-05 18:24:29 +0100

    Header and footer responsiveness

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -2,16 +2,18 @@
 
 import "./footer.css";
 import { Link } from 'react-router-dom';
+import { CiHome } from "react-icons/ci";
+import { CiCircleInfo } from "react-icons/ci";
 
 export const Footer = () => {
     return (
         <div className="footer">
-            <h2 className="footer-title"><Link to="/">Open AIr LOGO?</Link></h2>
+            <h2 className="footer-title"><Link to="/">Open AIr LOGO</Link></h2>
             <p>Crafted by Emmy Dieden and Idah Collin <a href="https://www.technigo.io/web-development-boot-camp?utm_source=google&utm_medium=cpc&utm_campaign=SE_Web_BC_PMaxFall2022&utm_adgroup=&utm_keyword=&device=c&gad_source=1&gclid=CjwKCAiAnL-sBhBnEiwAJRGignEdI26zEcDhy8PaJwGSgwQnfi6Q73Ac7nU-wog6RJga0mdReZc8VhoCdsYQAvD_BwE" target="_blank" rel="noopener noreferrer">Technigo</a> 23/24</p>
             <nav className="footer-nav">
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">Discover</Link></li>
+                    <li><Link to="/"><CiHome /></Link></li>
+                    <li><Link to="/about"><CiCircleInfo /></Link></li>
                 </ul>
             </nav>
         </div>
