2775a513ec536c3a184a5a80d70537523f9ad530
Lisa Dahlkar <lisa.dahlkar@gmail.com>
2024-01-06 19:34:43 +0100

    removed translation in footer sinced its not used

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import LanguageSwitcherFooter from "./LanguageSwitcherFooter";
 import "./css/footer.css";
 
 const Footer = () => {
 
-    const { t } = useTranslation();
 
     return (
         <div className="footer">
--- frontend/src/components/FooterMobile.jsx
+++ frontend/src/components/FooterMobile.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import LanguageSwitcherFooter from "./LanguageSwitcherFooter";
 import "./css/footermobile.css";
 
 const FooterMobile = () => {
 
-    const { t } = useTranslation();
 
     return (
         <div className="footermobile">
