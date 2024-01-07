4ac594680104e1a0a25d4d79b6c7815142bc6f63
Klaudia Wróblewska <kwroblewska0410@gmail.com>
2024-01-07 12:50:54 +0100

    made movies public

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,14 +1,9 @@
 import express from 'express'
 const router = express.Router()
-import {
-	getComingMovies,
-	getMovies,
-	getMovie,
-	postMovie,
-} from '../controllers/movieController'
-import { authenticateUser } from '../middleware/authenticateUser'
+import { getComingMovies, getMovies, getMovie, postMovie } from '../controllers/movieController'
+// import { authenticateUser } from '../middleware/authenticateUser'
 
-router.route('/').get(authenticateUser, getMovies).post(postMovie)
+router.route('/').get(getMovies).post(postMovie)
 router.route('/movie/:movieID').get(getMovie)
 router.route('/coming').get(getComingMovies)
 
