7a1c19cb63070d62e880cac4f107c7686499891f
Emmy Jansson <emmybengtsson@live.se>
2024-01-08 18:49:13 +0100

    footer import struggle

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 import { Buttons } from "../Buttons/Buttons";
 import "./Footer.css";
 
-// import linkedinIcon from "../assets/linkedin.png";
-// import githubIcon from "../assets/github.png";
+import linkedinIcon from "../assets/linkedin.png";
+import githubIcon from "../assets/github.png";
 
 export const Footer = () => {
   const handleButtonClick = () => {
@@ -15,10 +15,60 @@ export const Footer = () => {
       <div className="footer-text">
         <p>
           Technigo Web Developer Bootcamp Fall 2023 Final Project <br />
-          Frida Lindskog, Carolina Luna, Sandra Gustafsson
           <span>
-            Emmy
-            {/* <a
+            Frida Lindskog
+            <a
+              href="https://www.linkedin.com/in/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={linkedinIcon} alt="LinkedIn" />
+            </a>
+            <a
+              href="https://github.com/FridafridaL"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={githubIcon} alt="GitHub" />
+            </a>
+          </span>
+          <span>
+            Carolina Luna
+            <a
+              href="https://www.linkedin.com/in//"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={linkedinIcon} alt="LinkedIn" />
+            </a>
+            <a
+              href="https://github.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={githubIcon} alt="GitHub" />
+            </a>
+          </span>
+          <span>
+            Sandra Gustafsson
+            <a
+              href="https://www.linkedin.com/in//"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={linkedinIcon} alt="LinkedIn" />
+            </a>
+            <a
+              href="https://github.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={githubIcon} alt="GitHub" />
+            </a>
+          </span>
+          <span>
+            Emmy Jansson
+            <a
               href="https://www.linkedin.com/in/emmy-jansson-2104a3293/"
               target="_blank"
               rel="noopener noreferrer"
@@ -26,12 +76,12 @@ export const Footer = () => {
               <img src={linkedinIcon} alt="LinkedIn" />
             </a>
             <a
-              href="https://github.com/emmy"
+              href="https://github.com/EmmyLJ"
               target="_blank"
               rel="noopener noreferrer"
             >
               <img src={githubIcon} alt="GitHub" />
-            </a> */}
+            </a>
           </span>
         </p>
       </div>
