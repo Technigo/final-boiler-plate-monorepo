c4973efa1e967f6b6fcca9bb9b18f592019c9e15
Carolina Luna <kroluna@gmail.com>
2024-01-10 16:07:08 +0100

    s

--- frontend/src/components/Map/Mapcard.jsx
+++ frontend/src/components/Map/Mapcard.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import "../Storycard/Storycard.css";
 
 export const Mapcard = () => {
   // eslint-disable-next-line no-undef
