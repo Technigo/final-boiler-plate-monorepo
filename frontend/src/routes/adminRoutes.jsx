813b9e9b6a52df8537d00a1c42a840452055d8b4
mirelcac <141237662+mirelcac@users.noreply.github.com>
2024-01-10 02:26:26 +0100

    all admin pages open

--- frontend/src/routes/adminRoutes.jsx
+++ frontend/src/routes/adminRoutes.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import { AdminLogin } from "../pages/AdminLogin";
 import { AdminDashboard } from "../pages/AdminDashboard";
-// import { AdminRegister } from "../pages/adminPages/AdminRegister";
-// import { ListUsers } from "../pages/adminPages/ListUsers";
-// import { UpgradeUser } from "../pages/adminPages/UpgradeUser";
+import { AdminRegister } from "../pages/adminPages/AdminRegister";
+import { ListUsers } from "../pages/adminPages/ListUsers";
+import { UpgradeUser } from "../pages/adminPages/UpgradeUser";
 import { ManageCocktails } from "../pages/adminPages/ManageCocktails";
 
 const AdminRoutes = () => {
@@ -11,9 +11,9 @@ const AdminRoutes = () => {
         <Routes>
             <Route path="/login" element={<AdminLogin />} /> {/* Admin login page */}
             <Route path="/dashboard" element={<AdminDashboard />} /> {/* Landingpage with admin options*/}
-            {/*<Route path="/register" element={<AdminRegister />} /> {/* Register new admin */}
-            {/* <Route path="/users" element={<ListUsers />} /> {/* Get list of all registered users */}
-            {/* <Route path="/upgrade-user" element={<UpgradeUser />} /> {/* Upgrade user to admin */}
+            <Route path="/register" element={<AdminRegister />} /> {/* Register new admin */}
+            <Route path="/users" element={<ListUsers />} /> {/* Get list of all registered users */}
+            <Route path="/upgrade-user" element={<UpgradeUser />} /> {/* Upgrade user to admin */}
             <Route path="/manage-cocktails" element={<ManageCocktails />} /> {/* Page to manage cocktails - add, update and delete cocktails */}
             {/* Define other admin routes here */}
         </Routes>
