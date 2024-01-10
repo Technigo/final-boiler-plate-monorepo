3e74d62bfff0c3dbc3d306d5163f3e604bdc078d
Frida <frida@Educuratios-iMac.local>
2024-01-10 18:36:31 +0100

    Cleaned up the code in the Home-component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,79 +1,19 @@
-// // Import necessary dependencies and components.
-// import { useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { Link } from "react-router-dom";
+import { bookingStore } from '../store/bookingStore'
+
 import { Calendar } from '../components/Calendar'
 import { MovieList } from '../components/MovieList'
 import { Hero } from '../components/Hero'
-import { bookingStore } from '../store/bookingStore'
-// // Define the 'Home' functional component.
+
 export const Home = () => {
+
   const fetchShows = bookingStore((state) => (state.fetchAllShowTimes))
   fetchShows()
-  //   // Define text content for the heading and subheading.
-  //   const text = {
-  //     heading: "Vite + React + React Router + Minimal CSS",
-  //     subheading: "Home Page",
-  //     intro: "text here...",
-  //   };
 
-  //   // Access the 'handleLogout' function from the 'userStore'.
-  //   const storeHandleLogout = userStore((state) => state.handleLogout);
-
-  //   // Use the 'useNavigate' hook to programmatically navigate between routes.
-  //   const navigate = useNavigate();
-
-  //   // Get 'isLoggedIn' and 'accessToken' from the 'userStore'.
-  //   const { isLoggedIn, accessToken } = userStore();
-  //   console.log(isLoggedIn);
-  //   console.log(accessToken);
-
-  //   // useEffect hook to check user authentication status.
-  //   useEffect(() => {
-  //     if (!isLoggedIn) {
-  //       // If the user is not logged in, show an alert and navigate to the login route.
-  //       alert("no permission - here");
-  //       navigate("/"); // You can change this to the login route
-  //     }
-  //   }, [isLoggedIn]);
-
-  //   // Function to handle the click event of the logout button.
-  //   const onLogoutClick = () => {
-  //     storeHandleLogout(); // Call the 'handleLogout' function from 'userStore'.
-  //     // Additional logic after logout can be added here.
-  //     alert("Log out successful");
-  //     navigate("/"); // You can change this to the login route
-  //   };
-
-  // Render the component content.
   return (
     <>
       <Hero />
       <Calendar />
       <MovieList />
     </>
-    // <>
-    //       <nav>
-    //         {/* Create a navigation menu with links to various routes. */}
-    //         <ul className="app-ul">
-    //           <li className="app-li">
-    //             <Link to="/home">Home</Link>
-    //           </li>
-    //           <li className="app-li">
-    //             <Link to="/tasks">Tasks</Link>
-    //           </li>
-    //           <li className="app-li">
-    //             {/* Create a button for logging out and attach the 'onLogoutClick' event handler. */}
-    //             <button onClick={onLogoutClick}>Sign Out</button>
-    //           </li>
-    //         </ul>
-    //       </nav>
-    //       {/* Display the heading and subheading. */}
-    //       <h1 className="heading">{text.heading}</h1>
-    //       <h2>{text.subheading}</h2>
-    //       {/* (Note: 'text.intro' is not defined in the code.) */}
-    //       {/* Display additional content (text.intro is missing). */}
-    //       <p>{text.intro}</p>
-    //     </>
   )
 }
