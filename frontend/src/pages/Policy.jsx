c671287577778c9b1fb74adfbed3e51c1173be06
mariateresepettersson <mariateresepettersson>
2024-01-10 12:48:00 +0100

    Terms oage styled to be responsive

--- frontend/src/pages/Policy.jsx
+++ frontend/src/pages/Policy.jsx
@@ -1,6 +1,7 @@
 import { Footer } from "../components/Footer";
 import { Navbar } from "../components/Navbar";
 import { Heading } from "../components/reusableComponents/Heading";
+import "./policy.css";
 
 export const Policy = () => {
   return (
@@ -17,57 +18,66 @@ export const Policy = () => {
           { path: "/terms", name: "Terms" },
         ]}
       />
-      <div className="container">
-        <Heading
-          level={1}
-          text="Terms of Service"
-          aria-label="Terms of Service"
-        />
-        <h1>About Green Buddy Terms of Service</h1>
-        <ul>
-          <li>
-            Acceptance of Terms: By using the Green Buddy app, you agree to
-            abide by these terms and conditions.
-          </li>
-          <li>
-            Intellectual Property: You must respect Green Buddy&apos;s
-            intellectual property rights, and you are not allowed to reproduce
-            the service elsewhere.
-          </li>
-          <li>
-            Content Posting: When creating an ad or sharing content on the
-            Service, you must be entitled to post the content, either as the
-            owner or with the rights holder&apos;s permission. Content depicting
-            anything illegal is strictly prohibited.
-          </li>
+      <div className="main-container">
+        <div className="main-wrapper">
+          <Heading
+            level={1}
+            text="Terms of Service"
+            aria-label="Terms of Service"
+            className="heading-terms"
+          />
+          <Heading
+            level={2}
+            text="About Green Buddy Terms of Service"
+            aria-label="About Green Buddy Terms of Service"
+            className="heading-terms"
+          />
+          <div className="terms-wrapper">
+            <ul>
+              <li>
+                <span>Acceptance of Terms:</span> By using the Green Buddy app, you agree to
+                abide by these terms and conditions.
+              </li>
+              <li>
+                <span>Intellectual Property:</span> You must respect Green Buddy&apos;s
+                intellectual property rights, and you are not allowed to reproduce
+                the service elsewhere.
+              </li>
+              <li>
+                <span>Content Posting:</span> When creating an ad or sharing content on the
+                Service, you must be entitled to post the content, either as the
+                owner or with the rights holder&apos;s permission. Content depicting
+                anything illegal is strictly prohibited.
+              </li>
 
-          <li>
-            Limited Responsibility: Green Buddy has limited responsibility
-            for the availability, performance, and correctness of information
-            provided through the Service.
-          </li>
-          <li>
-            Privacy Policy: You should be aware that Green Buddy handles
-            personal information according to the current Privacy Policy.
-          </li>
-          <li>
-            Changes to Terms: Green Buddy reserves the right to update these
-            terms, and users will be notified of any changes.
-          </li>
-          <li>
-            Termination: Green Buddy reserves the right to terminate user
-            accounts for violations of these terms.
-          </li>
-          <li>
-            Dispute Resolution: Any disputes will be resolved through
-            arbitration in accordance with applicable laws.
-          </li>
-
-          <p>
+              <li>
+                <span>Limited Responsibility:</span> Green Buddy has limited responsibility
+                for the availability, performance, and correctness of information
+                provided through the Service.
+              </li>
+              <li>
+                <span>Privacy Policy:</span> You should be aware that Green Buddy handles
+                personal information according to the current Privacy Policy.
+              </li>
+              <li>
+                <span>Changes to Terms:</span> Green Buddy reserves the right to update these
+                terms, and users will be notified of any changes.
+              </li>
+              <li>
+                <span>Termination:</span> Green Buddy reserves the right to terminate user
+                accounts for violations of these terms.
+              </li>
+              <li>
+                <span>Dispute Resolution:</span> Any disputes will be resolved through
+                arbitration in accordance with applicable laws.
+              </li>
+            </ul>
+            <p>
             By using the Green Buddy app, you acknowledge that you have read and
             understood these terms and agree to be bound by them.
           </p>
-        </ul>
+          </div>
+        </div>
       </div>
       <Footer />
     </>
