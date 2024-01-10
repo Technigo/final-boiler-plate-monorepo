65a07498fdef90abb4f386cd14e6884427b4d57a
elbines <141184135+elbines@users.noreply.github.com>
2024-01-10 16:22:28 +0100

    one test image working, home and cocktails displaying test recipes. no styling

--- frontend/src/pages/Cocktails.jsx
+++ frontend/src/pages/Cocktails.jsx
@@ -1,9 +1,45 @@
 import styles from './Cocktails.module.css';
-
+import { Text } from '../UI/Typography';
+import React, { useState, useEffect } from 'react';
 
 export const Cocktails = () => {
+    const [cocktails, setCocktails] = useState([]);
+
+    useEffect(() => {
+        fetch('https://cbc-uvko.onrender.com/cocktails')
+            .then(response => response.json())
+            .then(data => {
+                const recentCocktails = data.sort((a, b) => new Date(b.date) - new Date(a.date)).slice(0, 6);
+                setCocktails(recentCocktails);
+            })
+            .catch(error => {
+                console.error('Error fetching data: ', error);
+            });
+    }, []);
+
     return (
         <div className={styles.wrapper}>
-            <h1 className={styles.h1}>THIS SHOULD BE A PAGE FOR COCKTAILS!</h1>;
-        </div>)
-}
+            <Text type="H1" className={styles.h1}>LATEST COCKTAILS</Text>
+            {cocktails.map(cocktail => (
+                <div key={cocktail._id}>
+                    {cocktail.imageUrl && (
+                        <img src={`https://cbc-uvko.onrender.com/${cocktail.imageUrl}`} alt={cocktail.name} className={styles.cocktailImage} />
+                    )}
+                    <h2>{cocktail.name}</h2>
+                    <p>Primary Liquor: {cocktail.primaryLiquor}</p>
+                    <p>Ingredients: {cocktail.ingredients.join(', ')}</p>
+                    <p>Ingredients Count: {cocktail.ingredientsCount}</p>
+                    <p>Difficulty: {cocktail.difficulty}</p>
+                    <p>Instructions: {cocktail.instructions}</p>
+                    <p>Category: {cocktail.category}</p>
+                    <p>Occasions: {cocktail.occasion.join(', ')}</p>
+                    <p>Creator: {cocktail.creator}</p>
+                    <p>Servings: {cocktail.servings}</p>
+                    <p>Prep Time: {cocktail.prepTime}</p>
+                    <p>Description: {cocktail.description}</p>
+                    <p>Strength: {cocktail.strength}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
--- frontend/src/pages/HomeCBC.jsx
+++ frontend/src/pages/HomeCBC.jsx
@@ -1,47 +1,43 @@
 import styles from './HomeCBC.module.css';
+import { Text } from '../UI/Typography';
 import React, { useState, useEffect } from 'react';
 
 export const HomeCBC = () => {
-    const [cocktails, setCocktails] = useState([]);
+    const [featuredCocktails, setFeaturedCocktails] = useState([]);
+    // Replace with the actual IDs of the cocktails you want to feature
+    const featuredIds = ['659d4d45faca05d6df658656', '6597a5cc65dea63aba7e1999'];
 
     useEffect(() => {
         fetch('https://cbc-uvko.onrender.com/cocktails')
             .then(response => response.json())
             .then(data => {
-                const recentCocktails = data.sort((a, b) => new Date(b.date) - new Date(a.date)).slice(0, 6);
-                setCocktails(recentCocktails);
+                // Filter the cocktails to only include those with IDs in featuredIds
+                const selectedCocktails = data.filter(cocktail => featuredIds.includes(cocktail._id));
+                setFeaturedCocktails(selectedCocktails);
             })
             .catch(error => {
                 console.error('Error fetching data: ', error);
-                // Handle the error state appropriately
+                // Optionally, handle the error state in the UI
             });
-    }, []);
+    }, []); // Empty dependency array ensures this effect runs once after the initial render
 
     return (
         <div className={styles.wrapper}>
-            <h1 className={styles.h1}>CONNECTING MAIN PAGE!</h1>
-            <div>
-                {cocktails.map(cocktail => (
-                    <div key={cocktail._id}>
-                        {cocktail.imageUrl && (
-                            <img src={`https://cbc-uvko.onrender.com/${cocktail.imageUrl}`} alt={cocktail.name} className={styles.cocktailImage} />
-                        )}
-                        <h2>{cocktail.name}</h2>
-                        <p>Primary Liquor: {cocktail.primaryLiquor}</p>
-                        <p>Ingredients: {cocktail.ingredients.join(', ')}</p>
-                        <p>Ingredients Count: {cocktail.ingredientsCount}</p>
-                        <p>Difficulty: {cocktail.difficulty}</p>
-                        <p>Instructions: {cocktail.instructions}</p>
-                        <p>Category: {cocktail.category}</p>
-                        <p>Occasions: {cocktail.occasion.join(', ')}</p>
-                        <p>Creator: {cocktail.creator}</p>
-                        <p>Servings: {cocktail.servings}</p>
-                        <p>Prep Time: {cocktail.prepTime}</p>
-                        <p>Description: {cocktail.description}</p>
-                        <p>Strength: {cocktail.strength}</p>
-                    </div>
-                ))}
-            </div>
+            <Text type="H1" className={styles.h1}>Featured Cocktails</Text>
+            {featuredCocktails.map(cocktail => (
+                <div key={cocktail._id} className={styles.cocktail}>
+                    {cocktail.imageUrl && (
+                        <img src={`https://cbc-uvko.onrender.com/${cocktail.imageUrl}`} alt={cocktail.name} className={styles.cocktailImage} />
+                    )}
+                    <h2>{cocktail.name}</h2>
+                    <p>⏲️: {cocktail.prepTime}</p>
+                    <p>🌟: {cocktail.strength}</p>
+                    <p>⚡: {cocktail.strength}</p>
+                    <p>🏷️ : {cocktail.tags}</p>
+                </div>
+            ))}
         </div>
     );
 };
+
+export default HomeCBC;
