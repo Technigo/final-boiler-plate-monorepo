3bdd55b1cf7002c3053faec969cb38f15c67eee3
elbines <141184135+elbines@users.noreply.github.com>
2024-01-11 13:35:03 +0100

    Updated multer config for array fields and file uploads in cocktail routes and controllers

--- backend/controllers/cocktailController.js
+++ backend/controllers/cocktailController.js
@@ -13,8 +13,10 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({ storage: storage });
-
+const upload = multer({ storage: storage }).fields([
+    { name: 'image', maxCount: 1 },
+  ]);
+  
 // Middleware to handle image upload
 const addImageToCocktail = (req, res, next) => {
     upload.single('image')(req, res, (error) => {
@@ -116,59 +118,39 @@ export const getCocktailByIdController = async (req, res) => {
     }
 };
 
-// Create a new cocktail with image upload
-export const addCocktailController = [
-    upload.single('image'),//multer should be used directly here
-    async (req, res) => {
-        //check admin role:
-        if (req.admin.role !== 'admin') {
-            return res.status(403).json({ message: 'Access denied' });
-        }
-        console.log(req.body); // Log the body to see the form data
-        console.log(req.file); // Log the file to see the image data
-
-      try {
-        // If the image is uploaded successfully, multer adds a file property to the request object.
-        if (req.file) {
-          req.body.imageUrl = req.file.path; // Save the path of the uploaded file in the imageUrl field
-        }
-
-        //Add time and date to the reicpe
-        req.body.date = new Date();
-  
-        const newCocktail = new Cocktails(req.body);
-        await newCocktail.save();
-        res.status(201).json(newCocktail);
-      } catch (error) {
-        res.status(400).json({ message: error.message });
+  export const addCocktailController = async (req, res) => {
+      // Check admin role first
+      if (req.admin.role !== 'admin') {
+          return res.status(403).json({ message: 'Access denied' });
       }
-    }
-  ];
+  
+      // Convert any single-entry fields to arrays if necessary
+      const arrayFields = ['allLiquors', 'ingredients', 'InspiredByCreator', 'occasion', 'flavorProfile', 'tags'];
+      arrayFields.forEach(field => {
+          if (req.body[field] && !Array.isArray(req.body[field])) {
+              req.body[field] = [req.body[field]];
+          }
+      });
+  
+      // If an image was uploaded, multer will add the file information to req.files
+      if (req.files && req.files['image']) {
+          req.body.imageUrl = req.files['image'][0].path;
+      }
+  
+      try {
+          // Add time and date to the recipe
+          req.body.date = new Date();
+  
+          // Create and save the new cocktail
+          const newCocktail = new Cocktails(req.body);
+          await newCocktail.save();
+          res.status(201).json(newCocktail);
+      } catch (error) {
+          res.status(400).json({ message: error.message });
+      }
+  };
+  
 
-// exports.addCocktailController = [
-//     upload.single('image'),
-//     async (req, res) => {
-//       console.log('Body:', req.body); // Should log form fields other than files
-//       console.log('File:', req.file); // Should log file information if a file is uploaded
-  
-//       try {
-//         if (req.file) {
-//           req.body.imageUrl = req.file.path;
-//         }
-  
-//         console.log('Request after processing:', req.body);
-  
-//         const newCocktail = new Cocktails(req.body);
-//         await newCocktail.save();
-//         res.status(201).json(newCocktail);
-//       } catch (error) {
-//         console.error('Error:', error);
-//         res.status(400).json({ message: error.message });
-//       }
-//     }
-//   ];
-  
-  
 // Update a cocktail by ID with image upload
 export const updateCocktailController = [addImageToCocktail, async (req, res) => {
     // Check admin role:
@@ -304,197 +286,3 @@ export const deleteCocktailController = async (req, res) => {
 // Controller Function: deleteCocktailController
 // Postman Setup: DELETE - body-none - http://localhost:3000/cocktails/:id
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const Cocktails = require('../models/Cocktails');
-
-// // Controller functions, get all cocktails
-// exports.getCocktailsController = async (req, res) => {
-//     try {
-//         const cocktails = await Cocktails.find();
-//         res.json(cocktails);
-//     } catch (error) {
-//         res.status(500).send(error.message);
-//     }
-// };
-
-// // Get a single cocktail by ID
-// exports.getCocktailByIdController = async (req, res) => {
-//     try {
-//         const cocktail = await Cocktails.findById(req.params.id); // Use 'Cocktails' instead of 'Cocktail'
-//         if (!cocktail) {
-//             return res.status(404).send('Cocktail not found');
-//         }
-//         res.json(cocktail);
-//     } catch (error) {
-//         res.status(500).send(error.message);
-//     }
-// };
-
-// // Create a new cocktail
-// exports.addCocktailController = async (req, res) => {
-//     try {
-//         const newCocktail = new Cocktails(req.body); // Use 'Cocktails' instead of 'Cocktail'
-//         await newCocktail.save();
-//         res.status(201).send(newCocktail);
-//     } catch (error) {
-//         res.status(400).send(error.message);
-//     }
-// };
-
-// // Update a cocktail by ID
-// exports.updateCocktailController = async (req, res) => {
-//     try {
-//         const cocktail = await Cocktails.findByIdAndUpdate(req.params.id, req.body, { new: true }); // Use 'Cocktails' instead of 'Cocktail'
-//         if (!cocktail) {
-//             return res.status(404).send('Cocktail not found');
-//         }
-//         res.json(cocktail);
-//     } catch (error) {
-//         res.status(400).send(error.message);
-//     }
-// };
-
-// // Delete a cocktail by ID
-// exports.deleteCocktailController = async (req, res) => {
-//     try {
-//         const cocktail = await Cocktails.findByIdAndDelete(req.params.id); // Use 'Cocktails' instead of 'Cocktail'
-//         if (!cocktail) {
-//             return res.status(404).send('Cocktail not found');
-//         }
-//         res.send('Cocktail deleted successfully');
-//     } catch (error) {
-//         res.status(500).send(error.message);
-//     }
-// };
-
-//POSTMAN TESTING
-
-////// Get All Cocktails//////
-// Endpoint: /cocktails
-// HTTP Method: GET
-// Description: Retrieves a list of all cocktails.
-// Controller Function: getCocktailsController
-// Postman Setup: GET - body-none - http://localhost:3000/cocktails
-
-
-
-////// Get Cocktail by ID //////
-// Endpoint: /cocktails/:id
-// HTTP Method: GET
-// Description: Retrieves a single cocktail by its ID.
-// Controller Function: getCocktailByIdController
-// Postman Setup: GET - body-none - http://localhost:3000/cocktails/:id
-
-
-
-////// Add New Cocktail //////
-// Endpoint: /cocktails
-// HTTP Method: POST
-// Description: Creates a new cocktail.
-// Controller Function: addCocktailController
-// Postman Setup: POST - body-raw-json - http://localhost:3000/cocktails
-// Example input:
-/*{
-    "name": "Classic Margarita",
-    "primaryLiquor": "Tequila",
-    "ingredients": ["2 oz Tequila", "1 oz Lime Juice", "1 oz Triple Sec", "Salt for rim", "Lime wedge for garnish"],
-    "instructions": "Rub the rim of the glass with the lime slice to make the salt stick to it. Shake the tequila, lime juice, and triple sec with ice and strain into the glass. Garnish with a lime wedge.",
-    "category": "Classic",
-    "color": "Yellow",
-    "ingredientsCount": 5,
-    "creator": "Jane Smith",
-    "occasion": ["Party", "Celebration"],
-    "difficulty": "Medium",
-    "flavorProfile": "Citrus and Strong",
-    "imageUrl": "path/to/classic-margarita-image.jpg",
-    "servings": 1,
-    "prepTime": "10 minutes",
-    "drinkware": "Margarita Glass",
-    "strength": "Strong",
-    "tags": ["Classic", "Citrus", "Strong"]
-  }*/
-  
-
-  
-////// Update Cocktail by ID
-
-// Endpoint: /cocktails/:id
-// HTTP Method: PUT
-// Description: Updates an existing cocktail by its ID.
-// Controller Function: updateCocktailController
-// Postman Setup: PUT - body-raw-json - http://localhost:3000/cocktails/:id
-// Example input:
-/*{
-    "name": "Fun Margarita",
-    "primaryLiquor": "Tequila",
-    "ingredients": ["2 oz Tequila", "1 oz Lime Juice", "1 oz Triple Sec", "Salt for rim", "Lime wedge for garnish"],
-    "instructions": "Rub the rim of the glass with the lime slice to make the salt stick to it. Shake the tequila, lime juice, and triple sec with ice and strain into the glass. Garnish with a lime wedge.",
-    "category": "Classic",
-    "color": "Yellow",
-    "ingredientsCount": 5,
-    "creator": "Jane Smith",
-    "occasion": ["Party", "Celebration"],
-    "difficulty": "Medium",
-    "flavorProfile": "Citrus and Strong",
-    "imageUrl": "path/to/classic-margarita-image.jpg",
-    "servings": 1,
-    "prepTime": "10 minutes",
-    "drinkware": "Margarita Glass",
-    "strength": "Strong",
-    "tags": ["Classic", "Citrus", "Strong"]
-  }*/
-  
-
-
-
-////// Delete Cocktail by ID //////
-
-// Endpoint: /cocktails/:id
-// HTTP Method: DELETE
-// Description: Deletes a cocktail by its ID.
-// Controller Function: deleteCocktailController
-// Postman Setup: DELETE - body-none - http://localhost:3000/cocktails/:id
--- backend/routes/cocktailRoutes.js
+++ backend/routes/cocktailRoutes.js
@@ -1,7 +1,5 @@
 import express from 'express';
-import multer from 'multer';
-import path from 'path';
-import { authenticateAdmin } from '../middleware/authenticateAdmin'; // Import middleware to add for protection
+import { authenticateAdmin } from '../middleware/authenticateAdmin';
 import {
     getCocktailsController,
     getCocktailByIdController,
@@ -9,53 +7,82 @@ import {
     updateCocktailController,
     deleteCocktailController
 } from '../controllers/cocktailController';
+import multer from 'multer';
+import path from 'path';
 
+const router = express.Router();
 
 // Set up multer for file uploads with updated storage configuration
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        // Make sure the directory 'uploads/' exists
         cb(null, 'uploads/');
     },
     filename: function (req, file, cb) {
-        // Append the timestamp and the original filename
         cb(null, `${Date.now()}-${file.originalname}`);
     }
 });
 
-const upload = multer({ storage: storage });
-
-const router = express.Router();
+// Configure multer to handle both single and array fields
+const upload = multer({ storage: storage }).fields([
+    { name: 'image', maxCount: 1 },
+    { name: 'allLiquors' },
+    { name: 'ingredients' },
+    { name: 'InspiredByCreator' },
+    { name: 'occasion' },
+    { name: 'flavorProfile' },
+    { name: 'tags' }
+    // array fields
+]);
 
 // Routes that handle image uploads with the updated multer middleware
-router.post('/', authenticateAdmin, upload.single('image'), addCocktailController);
-router.put('/:id', authenticateAdmin, upload.single('image'), updateCocktailController);
+router.post('/', authenticateAdmin, upload, addCocktailController);
+router.put('/:id', authenticateAdmin, upload, updateCocktailController);
 
-// Routes that do not handle image uploads remain the same
+// Other routes that do not require multer configuration for file upload
 router.get('/', getCocktailsController);
 router.get('/:id', getCocktailByIdController);
 router.delete('/:id', authenticateAdmin, deleteCocktailController);
 
 export default router;
 
-// import express from "express";
-// // Uncomment and use this when the middleware is ready
-// // import { authenticateAdmin } from '../middleware/authenticateAdmin';
 
+// import express from 'express';
+// import multer from 'multer';
+// import path from 'path';
+// import { authenticateAdmin } from '../middleware/authenticateAdmin'; // Import middleware to add for protection
 // import {
 //     getCocktailsController,
 //     getCocktailByIdController,
 //     addCocktailController,
 //     updateCocktailController,
 //     deleteCocktailController
-// } from '../controllers/cocktailController'; // Import your controller functions
+// } from '../controllers/cocktailController';
+
+
+// // Set up multer for file uploads with updated storage configuration
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         // Make sure the directory 'uploads/' exists
+//         cb(null, 'uploads/');
+//     },
+//     filename: function (req, file, cb) {
+//         // Append the timestamp and the original filename
+//         cb(null, `${Date.now()}-${file.originalname}`);
+//     }
+// });
+
+// const upload = multer({ storage: storage });
 
 // const router = express.Router();
 
-// router.get('/', /* authenticateAdmin, */ getCocktailsController);
-// router.get('/:id', /* authenticateAdmin, */ getCocktailByIdController);
-// router.post('/', /* authenticateAdmin, */ addCocktailController);
-// router.put('/:id', /* authenticateAdmin, */ updateCocktailController);
-// router.delete('/:id', /* authenticateAdmin, */ deleteCocktailController);
+// // Routes that handle image uploads with the updated multer middleware
+// router.post('/', authenticateAdmin, upload.single('image'), addCocktailController);
+// router.put('/:id', authenticateAdmin, upload.single('image'), updateCocktailController);
+
+// // Routes that do not handle image uploads remain the same
+// router.get('/', getCocktailsController);
+// router.get('/:id', getCocktailByIdController);
+// router.delete('/:id', authenticateAdmin, deleteCocktailController);
 
 // export default router;
+
