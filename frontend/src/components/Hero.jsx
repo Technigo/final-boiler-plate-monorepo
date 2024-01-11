8e925ccede7416fe36a27befa6c9e044ef0f545e
majoh23 <139242342+majoh23@users.noreply.github.com>
2024-01-11 08:57:11 +0100

    Update css hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,7 +5,7 @@ export const Hero = () => {
     <div className="relative bg-cover bg-center h-screen flex items-center justify-center p-8">
       <div className="absolute inset-0 bg-emerald-300"></div>
       <div className="max-w-4xl mx-auto text-white text-center z-10">
-        <h1 className="text-primary text-4xl sm:text-6xl font-bold mb-4 leading-3">
+        <h1 className="text-primary text-4xl sm:text-6xl font-bold mb-4 leading-normal">
           Be the driver of positive change. Discover an efficient way to travel,
           connect, and save.{" "}
         </h1>
