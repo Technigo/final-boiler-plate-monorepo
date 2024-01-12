386fb3163d7fd25bf8e518a7026c064705afbc3d
Linnea Johansson <Linneajoh@gmail.com>
2024-01-12 13:16:36 +0100

    another attempt

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,24 +1,9 @@
 import React from "react";
 import LanguageSwitcherFooter from "./LanguageSwitcherFooter";
+import './Footer.css';
 
 const Footer = () => {
-    const styles = {
-        root: { backgroundColor: "#f0e9dc", overflow: "hidden", padding: "10px" },
-        div: {
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          paddingLeft: "30px",
-        },
-        p: {
-          fontSize: "8px",
-          paddingTop: "10px",
-        },
-        img: {
-          width: "197px",
-        },
-      }
-      
+
     return (
         <div className="footer">
             <div className="footer-content">
