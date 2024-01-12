f98af8382885cdee16c3349f824ee8bec35b9fd5
Linnea Johansson <Linneajoh@gmail.com>
2024-01-12 13:03:53 +0100

    attempt to fix footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-import "./footer.css";
 import LanguageSwitcherFooter from "./LanguageSwitcherFooter";
 
 const Footer = () => {
-
+    const styles = {
+        root: { backgroundColor: "#f0e9dc", overflow: "hidden", padding: "10px" },
+        div: {
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          paddingLeft: "30px",
+        },
+        p: {
+          fontSize: "8px",
+          paddingTop: "10px",
+        },
+        img: {
+          width: "197px",
+        },
+      }
+      
     return (
         <div className="footer">
             <div className="footer-content">
