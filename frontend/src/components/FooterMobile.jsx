9fdaf5420c4c04b2a7290cffad27c9b7f9f56b2b
Linnea Johansson <Linneajoh@gmail.com>
2024-01-15 17:46:40 +0100

    inline styling for footer mobile

--- frontend/src/components/FooterMobile.jsx
+++ frontend/src/components/FooterMobile.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-import "./footermobile.css";
 import LanguageSwitcherFooter from "./LanguageSwitcherFooter";
 
 const FooterMobile = () => {
+    
+    const footermobileStyle = {
+        backgroundColor: "#f0e9dc",
+        overflow: "hidden",
+        padding: "10px",
+    };
+
+    const footermobileContentStyle = {
+        display: "flex",
+        flexDirection: "column",
+    };
+
+    const paragraphStyle = {
+        fontSize: "8px",
+    };
 
     return (
-        <div className="footermobile">
-            <div className="footermobile-content">
+        <div style={footermobileStyle}>
+            <div style={footermobileContentStyle}>
                 <LanguageSwitcherFooter />
-                <p>Copyright | All rights reserved | 2023</p>
+                <p style={paragraphStyle}>Copyright | All rights reserved | 2023</p>
             </div>
         </div>
     );
