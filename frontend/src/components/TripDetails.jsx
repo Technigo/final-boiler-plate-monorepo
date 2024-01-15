c09a911b9dc9adb6a36506cc07a812020d322107
andrkavr <72137459+andrkavr@users.noreply.github.com>
2024-01-15 13:15:33 +0100

    changed tripdetails so that user cannot join trips that they themselves have posted

--- frontend/src/components/TripDetails.jsx
+++ frontend/src/components/TripDetails.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { userStore } from "../stores/userStore";
 
 export const TripDetails = () => {
   const { tripId } = useParams();
+  const { loggedInUserId } = userStore();
   const [selectedTrip, setSelectedTrip] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState("");
@@ -47,13 +49,13 @@ export const TripDetails = () => {
 
   return (
     <>
+      {console.log(selectedTrip)}
       <div className="sm:mt-8 mt-2 mb-8 space-y-4 mx-auto max-w-screen-lg">
         <h1 className="text-lg font-md">Trip details</h1>
         <div className="grid grid-cols-12 gap-4 bg-secondary mb-8 p-4 rounded-lg">
           <div className="col-span-12 flex justify-center p-4">
             <div className="trip-info">
-              <div className="info-item">User: {selectedTrip.user}</div>
-              <div className="info-item">Username: {username}</div>
+              <div className="info-item">Username: {selectedTrip.username}</div>
               <div className="info-item">From: {selectedTrip.from}</div>
               <div className="info-item">To: {selectedTrip.to}</div>
               <div className="info-item">Date: {selectedTrip.date}</div>
@@ -64,13 +66,14 @@ export const TripDetails = () => {
               <div className="info-item">Make: {selectedTrip.make}</div>
               <div className="info-item">Model: {selectedTrip.model}</div>
               <div className="info-item">Message: {selectedTrip.message}</div>
-
-              <button
-                type="button"
-                className="bg-pink-400 rounded-full cursor-pointer hover:bg-cyan-800 text-white px-5 py-2 mt-8 font-semibold"
-              >
-                Join trip
-              </button>
+              {selectedTrip.user !== loggedInUserId && (
+                <button
+                  type="button"
+                  className="bg-pink-400 rounded-full cursor-pointer hover:bg-cyan-800 text-white px-5 py-2 mt-8 font-semibold"
+                >
+                  Join trip
+                </button>
+              )}
             </div>
           </div>
         </div>
