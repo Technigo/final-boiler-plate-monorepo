5cb93450f7fc18ac9dd5b9681bca83b7800587c6
Lisa Dahlkar <lisa.dahlkar@gmail.com>
2024-01-18 12:44:41 +0100

    errors with css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-//import "./header.css";
+import React from "react";
+import "./header.css";
+import { useTranslation } from "react-i18next";
 
 const Header = () => {
 
