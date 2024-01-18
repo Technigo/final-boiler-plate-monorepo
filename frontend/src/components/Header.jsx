dfe3afb5ccfedf31f95cc59537b5b8e93894c871
Lisa Dahlkar <lisa.dahlkar@gmail.com>
2024-01-18 12:42:06 +0100

    same as before

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-/*import "./header.css";*/
+import "./header.css";
 
 const Header = () => {
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,8 @@ import { App } from "./App.jsx";
 import "./App.css";
 import "./pages/habits.css";
 import "./components/createhabit.css";
-import "./components/header.css";
-import "./components/headermobile.css"
+//import "./components/header.css";
+//import "./components/headermobile.css"
 //import "./components/register.css";
 //import "./components/Startpage/loginform.css"
 //import "./components/Aboutus/aboutusinfo.css";
