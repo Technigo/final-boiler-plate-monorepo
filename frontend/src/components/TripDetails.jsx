9bae4dc20318fd4b85876bddfebceb69b32eb283
majoh23 <139242342+majoh23@users.noreply.github.com>
2024-01-18 21:27:30 +0100

    Updates to TripDetails

--- frontend/src/components/TripDetails.jsx
+++ frontend/src/components/TripDetails.jsx
@@ -120,15 +120,14 @@ export const TripDetails = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-6 h-6">
+                className="w-6 h-6 mr-1">
                 <path
                   fillRule="evenodd"
                   d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
                   clipRule="evenodd"
                 />
               </svg>
-            </span>{" "}
-            &nbsp;
+            </span>
             {selectedTrip.username}
           </div>
 
@@ -186,11 +185,23 @@ export const TripDetails = () => {
               )}
           </div>
         </div>
-        <div>
+        <div className="flex items-center">
           <Link
             to="/trips"
-            className="text-blue-500 text-md hover:text-blue-700 focus:outline-none focus:ring focus:border-blue-300">
-            {" "}
+            className="text-blue-500 text-md hover:text-blue-700 focus:outline-none focus:ring focus:border-blue-300 flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6 mr-1">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
             Back to Trips
           </Link>
         </div>
