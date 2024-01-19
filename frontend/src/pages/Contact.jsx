6772bc577303c099b3a4837cee7b9d7a76fa6c3c
majoh23 <139242342+majoh23@users.noreply.github.com>
2024-01-19 11:01:33 +0100

    Code cleaning

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const Contact = () => {
   return (
     <div
@@ -9,24 +7,27 @@ export const Contact = () => {
       flex flex-col
       bg-secondary
       space-y-4
-      justify-center"
-    >
+      justify-center">
       Email us:
-      <a href="mailto:info@piggyback.com" className="text-green-900 hover:underline ">
+      <a
+        href="mailto:info@piggyback.com"
+        className="text-green-900 hover:underline ">
         <i>info@piggyback.com</i>
       </a>
       <div className="flex space-x-2 justify-center">
-      <p>Follow us</p>
-        <a href="https://www.facebook.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-500 hover:underline">
-        Facebook
+        <p>Follow us</p>
+        <a
+          href="https://www.facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline">
+          Facebook
         </a>
-        <a href="https://www.linkedin.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-700 hover:underline">
+        <a
+          href="https://www.linkedin.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-700 hover:underline">
           Linkedin
         </a>
         {/* Add links for other social media platforms as needed */}
@@ -35,4 +36,3 @@ export const Contact = () => {
     </div>
   );
 };
-
