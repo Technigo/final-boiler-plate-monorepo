5540dcec62873133f03528e631e0465749e57c7b
Frida K <frida.kristiansson@gmail.com>
2024-01-19 11:42:45 +0100

    Changed the movie-title margin in booking

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -47,7 +47,7 @@ export const Calendar = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     centerMode: true,
     responsive: [
       {
