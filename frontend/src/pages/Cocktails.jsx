88cb88d20bb5cba5b57ebcee893ec2bbd1f9602d
elbines <141184135+elbines@users.noreply.github.com>
2024-01-20 19:20:33 +0100

    search

--- frontend/src/pages/Cocktails.jsx
+++ frontend/src/pages/Cocktails.jsx
@@ -12,9 +12,11 @@ export const Cocktails = () => {
     const [selectedFilter, setSelectedFilter] = useState('');
     const [itemsToDisplay, setItemsToDisplay] = useState(6);
     const [totalCocktails, setTotalCocktails] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
+    const animationContainer = useRef(null);
+
     const handleFilterChange = (e) => {
         setSelectedFilter(e.target.value);
-        console.log("Selected filter:", e.target.value);
     };
     const filters = {
         'Liquor': ['Gin', 'Vodka', 'Tequila', 'Rum', 'Whiskey'],
@@ -22,22 +24,16 @@ export const Cocktails = () => {
         'Occasion': ['Christmas', 'Summer', 'New Year', 'Halloween'],
         'FlavorProfile': ['Sweet', 'Bitter', 'Sour', 'Spicy']
     };
-
-    const [isLoading, setIsLoading] = useState(true);
-    const animationContainer = useRef(null);
-
     useEffect(() => {
-        setIsLoading(true); // Start loading
-        // Load the Lottie animation from the public URL path
+        setIsLoading(true);
         const anim = lottie.loadAnimation({
-            container: animationContainer.current, // The DOM element to host the animation
+            container: animationContainer.current,
             renderer: 'svg',
             loop: true,
             autoplay: true,
-            path: '/animations/Animation - 1705760771667.json' // The path to the JSON file
+            path: '/animations/Animation - 1705760771667.json'
         });
-
-        return () => anim.destroy(); // Cleanup the animation on component unmount
+        return () => anim.destroy();
     }, []);
 
     useEffect(() => {
@@ -50,53 +46,51 @@ export const Cocktails = () => {
             query += (query ? '&' : '') + `${category.toLowerCase()}=${encodeURIComponent(value)}`;
         }
 
-        setIsLoading(true); // Set loading to true before fetching data
-
         fetch(`https://cbc-uvko.onrender.com/cocktails${query ? '?' + query : ''}`)
             .then(response => response.json())
             .then(data => {
                 setCocktails(data);
                 setDisplayedCocktails(data.slice(0, itemsToDisplay));
                 setTotalCocktails(data.length);
-                setIsLoading(false); // Set loading to false after data is received
+                setIsLoading(false);
             })
             .catch(error => {
                 console.error('Error fetching cocktails:', error);
-                setIsLoading(false); // Also set loading to false if there's an error
+                setIsLoading(false);
             });
-
-        // Dependencies for useEffect. If any of these values change, the effect will rerun
     }, [searchTerm, selectedFilter, itemsToDisplay]);
 
-
     const loadMoreCocktails = () => {
-        setItemsToDisplay(prev => prev + 6); //show 6 more for each new "load more"
+        setItemsToDisplay(prev => prev + 6);
     };
 
     return (
         <div className={styles.wrapper}>
-            {/* Lottie animation container */}
-            {isLoading ? (
+            {isLoading && (
                 <div>
                     <div ref={animationContainer} className={styles.lottieContainer}></div>
                     <Text type="H3" className={styles.h3Load}>PLEASE WAIT WHILE LOADING RECIPES</Text>
-                </div>) : (
-                <>
-                    {/* Search Input */}
+                </div>
+            )}
+            {!isLoading && (
+                <> <Text type="H1" className={styles.h1}>EXPLORE OUR COCKTAILS</Text>
+
                     <div className={styles.searchContainer}>
                         <input
                             type="text"
-                            placeholder="Search for cocktails..."
+                            placeholder=" Search for cocktails..."
                             value={searchTerm}
                             onChange={(e) => setSearchTerm(e.target.value)}
+                            className={styles.searchInput}
                         />
                     </div>
 
-                    {/* Dropdown */}
-                    <div className={styles.dropdowns}>
+                    {/* TO BE FIXED - FILTERING IN BACKEND */}
+                    {/* <div className={styles.dropdowns}>
                         <select
                             value={selectedFilter}
                             onChange={handleFilterChange}
+                            className={styles.dropDown}
                         >
                             <option value="">Select Filter</option>
                             {Object.entries(filters).map(([category, values]) => (
@@ -107,49 +101,36 @@ export const Cocktails = () => {
                                 </optgroup>
                             ))}
                         </select>
+                    </div> */}
+
+                    <div className={styles.gridContainer}>
+                        {displayedCocktails.map(cocktail => (
+                            <Link to={`/cocktail/${cocktail._id}`} key={cocktail._id} className={styles.cocktailLink}>
+                                <div>
+                                    {cocktail.imageUrl && (
+                                        <img src={cocktail.imageUrl} alt={cocktail.name} className={styles.cocktailImage} />
+                                    )}
+                                    {cocktail.name && <Text type="H3" className={styles.h3}>{cocktail.name}</Text>}
+                                    <Text type="SbodyText" className={styles.SbodyText}>⏲️: {cocktail.prepTime} | 🌟: {cocktail.difficulty}</Text>
+                                    <Text type="SbodyText" className={styles.SbodyText}>⚡: {cocktail.strength} | 🏷️ : {cocktail.tags.join(', ')}</Text>
+                                </div>
+                            </Link>
+                        ))}
                     </div>
 
-                    <Text type="H1" className={styles.h1}>EXPLORE OUR COCKTAILS</Text>
+                    {displayedCocktails.length < totalCocktails && (
+                        <div className={styles.loadMoreButtonContainer}>
+                            <StyledButton onClick={loadMoreCocktails}>
+                                <p>EXPLORE MORE</p>
+                                <p>COCKTAILS</p>
+                            </StyledButton>
+                        </div>
+                    )}
                 </>
-            )}
-
-            {/* Grid Container */}
-            {!isLoading && (
-                <div className={styles.gridContainer}>
-                    {displayedCocktails.map(cocktail => (
-                        // Wrap each cocktail with Link, clickable
-                        <Link to={`/cocktail/${cocktail._id}`} key={cocktail._id} className={styles.cocktailLink}>
-                            <div>
-                                {cocktail.imageUrl && (
-                                    <img src={cocktail.imageUrl} alt={cocktail.name} className={styles.cocktailImage} />
-                                )}
-                                {cocktail.name && <Text type="H3" className={styles.h3}>{cocktail.name}</Text>}
-                                <Text type="SbodyText" className={styles.SbodyText}>⏲️: {cocktail.prepTime} | 🌟: {cocktail.difficulty}</Text>
-                                <Text type="SbodyText" className={styles.SbodyText}>⚡: {cocktail.strength} | 🏷️ : {cocktail.tags.join(', ')}</Text>
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-            )}
-
-            {/* Load more button */}
-            {!isLoading && displayedCocktails.length < totalCocktails && (
-                <div className={styles.loadMoreButtonContainer}>
-                    <StyledButton onClick={loadMoreCocktails}>
-                        <p>EXPLORE MORE</p>
-                        <p>COCKTAILS</p>
-                    </StyledButton>
-                </div>
             )}
         </div>
     );
 };
-
-
-
-
-
-
 
 
 
@@ -266,3 +247,158 @@ export const Cocktails = () => {
 //         </div>
 //     );
 // };
+
+
+
+
+
+
+////// JUST IN CASE,  BEFORE CHANGING SAVE OLD //////
+
+// import { useState, useEffect, useRef } from 'react';
+// import { Link } from 'react-router-dom';
+// import styles from './Cocktails.module.css';
+// import { Text } from '../UI/Typography';
+// import { StyledButton } from '../UI/StyledButton';
+// import lottie from 'lottie-web';
+
+// export const Cocktails = () => {
+//     const [cocktails, setCocktails] = useState([]);
+//     const [displayedCocktails, setDisplayedCocktails] = useState([]);
+//     const [searchTerm, setSearchTerm] = useState('');
+//     const [selectedFilter, setSelectedFilter] = useState('');
+//     const [itemsToDisplay, setItemsToDisplay] = useState(6);
+//     const [totalCocktails, setTotalCocktails] = useState(0);
+//     const handleFilterChange = (e) => {
+//         setSelectedFilter(e.target.value);
+//         console.log("Selected filter:", e.target.value);
+//     };
+//     const filters = {
+//         'Liquor': ['Gin', 'Vodka', 'Tequila', 'Rum', 'Whiskey'],
+//         'Color': ['Red', 'Blue', 'Green', 'Yellow', 'Clear'],
+//         'Occasion': ['Christmas', 'Summer', 'New Year', 'Halloween'],
+//         'FlavorProfile': ['Sweet', 'Bitter', 'Sour', 'Spicy']
+//     };
+
+//     const [isLoading, setIsLoading] = useState(true);
+//     const animationContainer = useRef(null);
+
+//     useEffect(() => {
+//         setIsLoading(true); // Start loading
+//         // Load the Lottie animation from the public URL path
+//         const anim = lottie.loadAnimation({
+//             container: animationContainer.current, // The DOM element to host the animation
+//             renderer: 'svg',
+//             loop: true,
+//             autoplay: true,
+//             path: '/animations/Animation - 1705760771667.json' // The path to the JSON file
+//         });
+
+//         return () => anim.destroy(); // Cleanup the animation on component unmount
+//     }, []);
+
+//     useEffect(() => {
+//         let query = '';
+//         if (searchTerm) {
+//             query += `search=${encodeURIComponent(searchTerm)}`;
+//         }
+//         if (selectedFilter) {
+//             const [category, value] = selectedFilter.split(':');
+//             query += (query ? '&' : '') + `${category.toLowerCase()}=${encodeURIComponent(value)}`;
+//         }
+
+//         setIsLoading(true); // Set loading to true before fetching data
+
+//         fetch(`https://cbc-uvko.onrender.com/cocktails${query ? '?' + query : ''}`)
+//             .then(response => response.json())
+//             .then(data => {
+//                 setCocktails(data);
+//                 setDisplayedCocktails(data.slice(0, itemsToDisplay));
+//                 setTotalCocktails(data.length);
+//                 setIsLoading(false); // Set loading to false after data is received
+//             })
+//             .catch(error => {
+//                 console.error('Error fetching cocktails:', error);
+//                 setIsLoading(false); // Also set loading to false if there's an error
+//             });
+
+//         // Dependencies for useEffect. If any of these values change, the effect will rerun
+//     }, [searchTerm, selectedFilter, itemsToDisplay]);
+
+
+//     const loadMoreCocktails = () => {
+//         setItemsToDisplay(prev => prev + 6); //show 6 more for each new "load more"
+//     };
+
+//     return (
+//         <div className={styles.wrapper}>
+//             {/* Lottie animation container */}
+//             {isLoading ? (
+//                 <div>
+//                     <div ref={animationContainer} className={styles.lottieContainer}></div>
+//                     <Text type="H3" className={styles.h3Load}>PLEASE WAIT WHILE LOADING RECIPES</Text>
+//                 </div>) : (
+//                 <>
+//                     {/* Search Input */}
+//                     <div className={styles.searchContainer}>
+//                         <input
+//                             type="text"
+//                             placeholder="Search for cocktails..."
+//                             value={searchTerm}
+//                             onChange={(e) => setSearchTerm(e.target.value)}
+//                         />
+//                     </div>
+
+//                     {/* Dropdown */}
+//                     <div className={styles.dropdowns}>
+//                         <select
+//                             value={selectedFilter}
+//                             onChange={handleFilterChange}
+//                         >
+//                             <option value="">Select Filter</option>
+//                             {Object.entries(filters).map(([category, values]) => (
+//                                 <optgroup label={category} key={category}>
+//                                     {values.map(value => (
+//                                         <option key={value} value={`${category}:${value}`}>{value}</option>
+//                                     ))}
+//                                 </optgroup>
+//                             ))}
+//                         </select>
+//                     </div>
+
+//                     <Text type="H1" className={styles.h1}>EXPLORE OUR COCKTAILS</Text>
+//                 </>
+//             )}
+
+//             {/* Grid Container */}
+//             {!isLoading && (
+//                 <div className={styles.gridContainer}>
+//                     {displayedCocktails.map(cocktail => (
+//                         // Wrap each cocktail with Link, clickable
+//                         <Link to={`/cocktail/${cocktail._id}`} key={cocktail._id} className={styles.cocktailLink}>
+//                             <div>
+//                                 {cocktail.imageUrl && (
+//                                     <img src={cocktail.imageUrl} alt={cocktail.name} className={styles.cocktailImage} />
+//                                 )}
+//                                 {cocktail.name && <Text type="H3" className={styles.h3}>{cocktail.name}</Text>}
+//                                 <Text type="SbodyText" className={styles.SbodyText}>⏲️: {cocktail.prepTime} | 🌟: {cocktail.difficulty}</Text>
+//                                 <Text type="SbodyText" className={styles.SbodyText}>⚡: {cocktail.strength} | 🏷️ : {cocktail.tags.join(', ')}</Text>
+//                             </div>
+//                         </Link>
+//                     ))}
+//                 </div>
+//             )}
+
+//             {/* Load more button */}
+//             {!isLoading && displayedCocktails.length < totalCocktails && (
+//                 <div className={styles.loadMoreButtonContainer}>
+//                     <StyledButton onClick={loadMoreCocktails}>
+//                         <p>EXPLORE MORE</p>
+//                         <p>COCKTAILS</p>
+//                     </StyledButton>
+//                 </div>
+//             )}
+//         </div>
+//     );
+// };
+
