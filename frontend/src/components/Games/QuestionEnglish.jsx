bca60ae238977fbf250f276149dfbf97eea5418e
fridaforser <fridaforser@gmail.com>
2024-05-29 15:57:06 +0200

    Added timeout after answer to show correct answer and disable buttons
    Co-authored-by: Erika Wernbro <Tejpex@users.noreply.github.com>

--- frontend/src/components/Games/QuestionEnglish.jsx
+++ frontend/src/components/Games/QuestionEnglish.jsx
@@ -7,50 +7,48 @@ import englishData from "../../../data/EnglishGameData.json";
 export const Question = () => {
   const { english } = englishData
   const { score, setScore } = useScore()
-  const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random() * english.length))
+  const [randomNumber, setRandomNumber] = useState(4)
   const [message, setMessage] = useState("")
+  const [disableButton, setDisableButton] = useState(false)
 
   const answers = []
+
+  const generateAnswers = () => {
+    english[randomNumber].wrongAnswer.map((answer) => answers.push(answer))
+    answers.push(english[randomNumber].rightAnswer)
+    answers.sort()
+    /*for (let i = answers.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [answers[i], answers[j]] = [answers[j], answers[i]];
+    } return answers*/
+  }
+
+  generateAnswers()
+
+  const generateQuestion = () => {
+    setRandomNumber(Math.floor(Math.random() * english.length))
+    generateAnswers()
+    setDisableButton(false)
+    setMessage("")
+  }
   
-  useEffect(()=> {
-    const generateAnswers = () => {
-      english[randomNumber].wrongAnswer.map((answer) => answers.push(answer))
-      answers.push(english[randomNumber].rightAnswer)
-      for (let i = answers.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [answers[i], answers[j]] = [answers[j], answers[i]];
-      } return answers
-    }
-    generateAnswers(),[randomNumber]
-  })
+  useEffect(() => {
+    generateQuestion()
+  }, [])
 
   
   
-
-
-
-  //Kolla vilket svar användaren klickar på
-
+  //Takes users answer and compare it to right answer to generate right/wrong-message
   const handleChoice = (answer) => {
-    console.log(answer)
-    console.log("First", message)
     const rightAnswer = english[randomNumber].rightAnswer
     if (answer === rightAnswer) {
       setScore(score + 1)
-      //alert("Det var rätt! Bra jobbat!")
       setMessage("Det var rätt! Bra jobbat!")
-      
     } else {
-      //alert(`Det var tyvärr fel svar. Rätt svar var ${rightAnswer}. Du tryckte på ${answer}`)
       setMessage(`Det var tyvärr fel svar. Rätt svar var ${rightAnswer}.`)
     }
-    setTimeout(showAnswer, 2000)
-    console.log(score)
-    
-  }
-
-  const showAnswer = () => {
-    console.log("Show", message)
+    setDisableButton(true)
+    setTimeout(() => generateQuestion(), 4000)
   }
 
   return (
@@ -59,10 +57,11 @@ export const Question = () => {
       <QuestionCard>{english[randomNumber].question}</QuestionCard>
       <Answers>
         {answers.map((answer, index) => (
-          <AnswerButton key={index} value={answer} onClick={(event) => handleChoice(event.target.value)}>{answer}</AnswerButton>
+          <AnswerButton disabled={disableButton} key={index} value={answer} onClick={(event) => handleChoice(event.target.value)}>{answer}</AnswerButton>
         ))}
       </Answers>
-      <Message>{message}</Message>
+      {message && <Message>{message}</Message>}
+      <Score>Du har {score} rätt hittills</Score>
     </div>
   )
 }
@@ -89,8 +88,22 @@ const Answers = styled.div`
 const AnswerButton = styled.button`
   color: white;
   background-color: black;
+  border: none;
+
+  &:disabled {
+    cursor: default;
+    border: none;
+  }
 `;
 
 const Message = styled.p`
   color: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+`;
+
+const Score = styled.h3`
+  color: black;
 `;
