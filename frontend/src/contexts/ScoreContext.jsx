bd22522a1febd749ce8dc032bc5676bffb5271f4
fridaforser <fridaforser@gmail.com>
2024-05-30 11:59:34 +0200

    Added global math state

--- frontend/src/contexts/ScoreContext.jsx
+++ frontend/src/contexts/ScoreContext.jsx
@@ -4,19 +4,20 @@ const ScoreContext = createContext();
 
 export const ScoreProvider = ({ children }) => {
     const [score, setScore] = useState(0);
-
-
-
+    const [mathType, setMathType] = useState("")
 
     return (
     <ScoreContext.Provider
       value={{
         score,
         setScore,
+        mathType,
+        setMathType
       }}
     >
       {children}
     </ScoreContext.Provider>
   );
 };
-export const useScore = () => useContext(ScoreContext);
+
+export const useScore = () => useContext(ScoreContext)
