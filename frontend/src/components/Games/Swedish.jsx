e56845dfb1e4ceaa52b088582ca87ee5342d2d7d
fridaforser <fridaforser@gmail.com>
2024-06-04 20:17:16 +0200

    Added div

--- frontend/src/components/Games/Swedish.jsx
+++ frontend/src/components/Games/Swedish.jsx
@@ -3,12 +3,22 @@ import { SwedishQuestion } from "./SwedishQuestion.jsx";
 
 export const Swedish = () => {
   return (
-    <>
+    <SwedishGame>
       <SwedishTitle>Svenska</SwedishTitle>
       <SwedishQuestion />
-    </>
+    </SwedishGame>
   );
 };
+
+const SwedishGame = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin: 0 auto;
+`;
+
 const SwedishTitle = styled.h2`
   color: black;
 `;
