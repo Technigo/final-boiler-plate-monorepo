d38162e0870e51e792abb36eaee9466f78fe73a3
Tejpex <equis_angel@hotmail.com>
2024-06-05 18:27:27 +0200

    Seperated choice of math-game from actual game + refactored data

--- frontend/src/contexts/MathContext.jsx
+++ frontend/src/contexts/MathContext.jsx
@@ -5,117 +5,114 @@ import { createContext, useContext, useState } from "react"
 const MathContext = createContext()
 
 export const MathProvider = ({ children }) => {
-  const [mathType, setMathType] = useState("addition")
-  const [question, setQuestion] = useState("")
-  const [correctAnswer, setCorrectAnswer] = useState()
-  /*const [mathLevel, setMathLevel] = useState({
-    addition: 1,
-    subtraction: 1,
-    multiplication: 1,
-    division: 1,
-  })*/
-  const [mathScore, setMathScore] = useState({
-    addition: 0,
-    subtraction: 0,
-    multiplication: 0,
-    division: 0,
-  })
-  const [savedQuestion, setSavedQuestion] = useState({
-    addition: "",
-    subtraction: "",
-    multiplication: "",
-    division: "",
-  })
-  const [savedAnswer, setSavedAnswer] = useState({
-    addition: "",
-    subtraction: "",
-    multiplication: "",
-    division: "",
-  });
+  const [mathGame, setMathGame] = useState([
+    {
+      mathType: "Addera",
+      question: "",
+      correctAnswer: null,
+      level: 1,
+      score: 0,
+    },
+    {
+      mathType: "Subtrahera",
+      question: "",
+      correctAnswer: null,
+      level: 1,
+      score: 0,
+    },
+    {
+      mathType: "Multiplicera",
+      question: "",
+      correctAnswer: null,
+      level: 1,
+      score: 0,
+    },
+    {
+      mathType: "Dividera",
+      question: "",
+      correctAnswer: null,
+      level: 1,
+      score: 0,
+    }
+  ])
 
   //Generates different questions depending on the type of mathematical operation
   const generateAdditionQuestion = () => {
     const a = Math.floor(Math.random() * 100) + 1
     const b = Math.floor(Math.random() * 100) + 1
-    setCorrectAnswer(a + b)
-    setQuestion(`Vad är ${a}+${b}?`)
+    const newGame = [...mathGame]
+    newGame[0].question = `Vad är ${a}+${b}?`
+    newGame[0].correctAnswer = a + b
+
+    setMathGame(newGame)
   }
 
   const generateSubtractionQuestion = () => {
-    const a = Math.floor(Math.random() * 100) + 1;
-    const b = Math.floor(Math.random() * 100) + 1;
+    const a = Math.floor(Math.random() * 100) + 1
+    const b = Math.floor(Math.random() * 100) + 1
     if (a >= b) {
-      setCorrectAnswer(a - b);
-      setQuestion(`Vad är ${a}-${b}?`);
+      const newGame = [...mathGame]
+      newGame[1].question = `Vad är ${a}-${b}?`
+      newGame[1].correctAnswer = a - b
+
+      setMathGame(newGame)
     } else {
-      setCorrectAnswer(b - a);
-      setQuestion(`Vad är ${b}-${a}?`);
-    }
-  };
+      const newGame = [...mathGame]
+      newGame[1].question = `Vad är ${b}-${a}?`
+      newGame[1].correctAnswer = b - a
 
-  const generateMultiplicationQuestion = () => {
-    const a = Math.floor(Math.random() * 11);
-    const b = Math.floor(Math.random() * 11);
-    setCorrectAnswer(a * b);
-    setQuestion(`Vad är ${a}*${b}?`);
-  };
-
-  const generateDivisionQuestion = () => {
-    const a = Math.floor(Math.random() * 10) + 1;
-    const b = Math.floor(Math.random() * 11);
-    const c = a * b;
-    setCorrectAnswer(c / a);
-    setQuestion(`Vad är ${c}/${a}?`);
-  };
-
-  //Decides which type of problem to generate based on prop from Math.jsx
-  const generateQuestion = (type) => {
-    if (type === "addition" && savedQuestion.addition) {
-      setQuestion(savedQuestion.addition)
-      setCorrectAnswer(savedAnswer.addition)
-      setSavedQuestion({ ...savedQuestion, addition: "" })
-      setSavedAnswer({ ...savedAnswer, addition: "" })
-    } else if (type === "subtraction" && savedQuestion.subtraction) {
-      setQuestion(savedQuestion.subtraction)
-      setCorrectAnswer(savedAnswer.subtraction)
-      setSavedQuestion({ ...savedQuestion, subtraction: "" });
-      setSavedAnswer({ ...savedAnswer, subtraction: "" });
-    } else {
-      switch (type) {
-        case "addition":
-          console.log("New addition")
-          generateAdditionQuestion();
-          break;
-        case "subtraction":
-          generateSubtractionQuestion();
-          break;
-        case "multiplication":
-          generateMultiplicationQuestion();
-          break;
-        case "division":
-          generateDivisionQuestion();
-          break;
-        default:
-          generateAdditionQuestion();
-      }
+      setMathGame(newGame);
     }
   }
 
+  const generateMultiplicationQuestion = () => {
+    const a = Math.floor(Math.random() * 11)
+    const b = Math.floor(Math.random() * 11)
+   
+    const newGame = [...mathGame]
+    newGame[2].question = `Vad är ${a}*${b}?`
+    newGame[2].correctAnswer = a * b
+
+    setMathGame(newGame)
+  }
+
+  const generateDivisionQuestion = () => {
+    const a = Math.floor(Math.random() * 10) + 1
+    const b = Math.floor(Math.random() * 11)
+    const c = a * b
+
+    const newGame = [...mathGame];
+    newGame[3].question = `Vad är ${c}/${a}?`
+    newGame[3].correctAnswer = c / a
+
+    setMathGame(newGame)
+  }
+
+  //Decides which type of problem to generate based on prop from Math.jsx
+  const generateQuestion = (type) => {
+    switch (type) {
+      case 0:
+        generateAdditionQuestion();
+        break;
+      case 1:
+        generateSubtractionQuestion();
+        break;
+      case 2:
+        generateMultiplicationQuestion();
+        break;
+      case 3:
+        generateDivisionQuestion();
+        break;
+      default:
+        generateAdditionQuestion();
+    }
+  }
+  
   return (
     <MathContext.Provider
       value={{
-        mathType,
-        setMathType,
-        question,
-        setQuestion,
-        correctAnswer,
-        setCorrectAnswer,
-        mathScore,
-        setMathScore,
-        savedQuestion,
-        setSavedQuestion,
-        savedAnswer,
-        setSavedAnswer,
+        mathGame,
+        setMathGame,
         generateQuestion
       }}
     >
