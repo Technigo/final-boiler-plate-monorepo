1094588ecc4b94f4794abfdcd5e1296db44f6a38
fridaforser <fridaforser@gmail.com>
2024-06-12 15:02:03 +0200

    Styled footer to work with header image

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,20 +4,46 @@ import { Link } from "react-router-dom";
 export const Footer = () => {
     return (
       <FooterContainer>
-        <p>© Technigo Web Development Bootcamp 2024</p>
-        <Link to={`/om-oss`}>
-          <p>Om oss</p>
-        </Link>
+        <FooterDiv>
+          <p>© Technigo Web Development Bootcamp 2024</p>
+          <Link to={`/om-oss`}>
+            <p>Om oss</p>
+          </Link>
+          </FooterDiv>
       </FooterContainer>
     );
 };
 
 const FooterContainer = styled.div`
-  padding: 20px;
+  padding: 14px 20px;
   background-color: var(--daffodil);
   width: 100%;
-  bottom: 0;
   display: flex;
   flex-direction: row;
+  position: absolute;
+  bottom: 0;
+  margin: auto;
+  justify-content: center;
+  align-items: center;
+
+    @media (min-width: 1025px) {
+    padding: 20px 30px;
+    position: absolute;
+  
+  }
+`;
+
+const FooterDiv = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: row;
+  position: absolute;
   justify-content: space-between;
+  align-items: center;
+  font-size: 13px;
+
+    @media (min-width: 1025px) {
+    font-size: 15px;
+  
+  }
 `;
