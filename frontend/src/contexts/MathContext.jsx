84c6ef310f6760394c92c06184193e68dd6cdc6a
Tejpex <equis_angel@hotmail.com>
2024-06-12 12:35:47 +0200

    Added level-handling for multiplication and division

--- frontend/src/contexts/MathContext.jsx
+++ frontend/src/contexts/MathContext.jsx
@@ -38,17 +38,18 @@ export const MathProvider = ({ children }) => {
       score: 0,
       levelScore: 20,
     },
-  ])
+  ]);
 
   //Generates different questions depending on the type of mathematical operation
+  //ADDITION
   const generateAdditionQuestion = () => {
-    let a = 0
-    let b = 0
+    let a = 0;
+    let b = 0;
 
     //Setting different numbers depending on level
     if (mathGame[0].level === 1) {
-      a = Math.floor(Math.random() * 20) + 1
-      b = Math.floor(Math.random() * 20) + 1
+      a = Math.floor(Math.random() * 20) + 1;
+      b = Math.floor(Math.random() * 20) + 1;
     } else if (mathGame[0].level === 2) {
       a = Math.floor(Math.random() * 50) + 1;
       b = Math.floor(Math.random() * 50) + 1;
@@ -58,15 +59,16 @@ export const MathProvider = ({ children }) => {
     }
 
     //Using numbers to create question and answer
-    const newGame = [...mathGame]
-    newGame[0].question = `Vad är ${a}+${b}?`
-    newGame[0].correctAnswer = a + b
-    setMathGame(newGame)
-  }
+    const newGame = [...mathGame];
+    newGame[0].question = `Vad är ${a}+${b}?`;
+    newGame[0].correctAnswer = a + b;
+    setMathGame(newGame);
+  };
 
+  //SUBTRACTION
   const generateSubtractionQuestion = () => {
-    let a = 0
-    let b = 0
+    let a = 0;
+    let b = 0;
 
     //Setting different numbers depending on level
     if (mathGame[1].level === 1) {
@@ -85,38 +87,62 @@ export const MathProvider = ({ children }) => {
       const newGame = [...mathGame];
       newGame[1].question = `Vad är ${a}-${b}?`;
       newGame[1].correctAnswer = a - b;
-
       setMathGame(newGame);
     } else {
       const newGame = [...mathGame];
       newGame[1].question = `Vad är ${b}-${a}?`;
       newGame[1].correctAnswer = b - a;
-
       setMathGame(newGame);
     }
   }
 
+  //MULTIPLICATION
   const generateMultiplicationQuestion = () => {
-    const a = Math.floor(Math.random() * 11)
-    const b = Math.floor(Math.random() * 11)
-   
-    const newGame = [...mathGame]
-    newGame[2].question = `Vad är ${a}*${b}?`
-    newGame[2].correctAnswer = a * b
+    let a = 0;
+    let b = 0;
 
-    setMathGame(newGame)
+    //Setting different numbers depending on level
+    if (mathGame[2].level === 1) {
+      a = Math.floor(Math.random() * 5) + 1;
+      b = Math.floor(Math.random() * 5) + 1;
+    } else if (mathGame[2].level === 2) {
+      a = Math.floor(Math.random() * 5) + 1;
+      b = Math.floor(Math.random() * 11);
+    } else {
+      a = Math.floor(Math.random() * 10) + 1;
+      b = Math.floor(Math.random() * 11);
+    }
+
+    //Using numbers to create question and answer
+    const newGame = [...mathGame];
+    newGame[2].question = `Vad är ${a}*${b}?`;
+    newGame[2].correctAnswer = a * b;
+    setMathGame(newGame);
   }
 
+  //DIVISION
   const generateDivisionQuestion = () => {
-    const a = Math.floor(Math.random() * 10) + 1
-    const b = Math.floor(Math.random() * 11)
-    const c = a * b
+    let a = 0;
+    let b = 0;
 
+    //Setting different numbers depending on level
+    if (mathGame[3].level === 1) {
+      a = Math.floor(Math.random() * 5) + 1;
+      b = Math.floor(Math.random() * 5) + 1;
+    } else if (mathGame[3].level === 2) {
+      a = Math.floor(Math.random() * 5) + 1;
+      b = Math.floor(Math.random() * 11);
+    } else {
+      a = Math.floor(Math.random() * 10) + 1;
+      b = Math.floor(Math.random() * 11);
+    }
+
+    //Using numbers to create question and answer
+    const c = a * b;
     const newGame = [...mathGame];
-    newGame[3].question = `Vad är ${c}/${a}?`
-    newGame[3].correctAnswer = c / a
-
-    setMathGame(newGame)
+    newGame[3].question = `Vad är ${c}/${a}?`;
+    newGame[3].correctAnswer = c / a;
+    setMathGame(newGame);
   }
 
   //Decides which type of problem to generate based on prop from Math.jsx
@@ -124,10 +150,10 @@ export const MathProvider = ({ children }) => {
     //use next line for testing/demoing (only three questions before level-change)
     if (mathGame[type].score >= 3) {
     //if (mathGame[type].score >= mathGame[type].levelScore) {
-      const newGame = [...mathGame]
-      newGame[type].level = mathGame[type].level + 1
-      newGame[type].score = 0
-      setMathGame(newGame)
+      const newGame = [...mathGame];
+      newGame[type].level = mathGame[type].level + 1;
+      newGame[type].score = 0;
+      setMathGame(newGame);
     }
     switch (type) {
       case 0:
@@ -146,13 +172,13 @@ export const MathProvider = ({ children }) => {
         generateAdditionQuestion();
     }
   }
-  
+
   return (
     <MathContext.Provider
       value={{
         mathGame,
         setMathGame,
-        generateQuestion
+        generateQuestion,
       }}
     >
       {children}
